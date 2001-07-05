e7f8947bb55e555e1e1524a924616e348fc3020b
nouaya <nouaya>
2001-07-05 15:56:18 +0000

    add support of upgrade

---
+++ upgrade2-2to2-3.php
@@ -0,0 +1,135 @@
+<?php
+/* upgrade from 2.2 to 2.3 */
+include "config.php";
+include $babInstallPath."index.php";
+
+function upgrade()
+{
+$ret = "";
+
+$db = new db_mysql();
+
+$req = "ALTER TABLE groups ADD manager INT (11) UNSIGNED not null AFTER vacation";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>groups</b> table failed !<br>";
+	return $ret;
+	}
+$req = "ALTER TABLE users_log ADD lastlog DATETIME not null AFTER datelog";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>users_log</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "CREATE TABLE categoriescal (";
+$req .= "id TINYINT (2) UNSIGNED not null AUTO_INCREMENT,";
+$req .= "name VARCHAR (60) not null,";
+$req .= "description VARCHAR (255) not null,";
+$req .= "bgcolor VARCHAR (6) not null,";
+$req .= "id_group INT (11) UNSIGNED not null,";
+$req .= "PRIMARY KEY (id)";
+$req .= ")";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>categoriescal</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "CREATE TABLE resourcescal (";
+$req .= "id INT (11) UNSIGNED not null AUTO_INCREMENT,";
+$req .= "name VARCHAR (60) not null,";
+$req .= "description VARCHAR (255) not null,";
+$req .= "id_group INT (11) UNSIGNED not null,";
+$req .= "PRIMARY KEY (id)";
+$req .= ")"; 
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>resourcescal</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "CREATE TABLE cal_events (";
+$req .= "id INT (11) UNSIGNED not null AUTO_INCREMENT,";
+$req .= "id_cal INT (11) UNSIGNED not null,";
+$req .= "title VARCHAR (255) not null,";
+$req .= "description TEXT not null,";
+$req .= "start_date DATE not null,";
+$req .= "start_time TIME not null,";
+$req .= "end_date DATE not null,";
+$req .= "end_time TIME not null,";
+$req .= "id_cat INT (11) UNSIGNED not null,";
+$req .= "PRIMARY KEY (id)";
+$req .= ")";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>cal_events</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "CREATE TABLE calendar (";
+$req .= "id int(11) unsigned NOT NULL auto_increment,";
+$req .= "owner int(11) unsigned DEFAULT '0' NOT NULL,";
+$req .= "actif enum('Y','N') DEFAULT 'Y' NOT NULL,";
+$req .= "type TINYINT (2) not null,";
+$req .= "PRIMARY KEY (id)";
+$req .= ")";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>calendar</b> table failed !<br>";
+	return $ret;
+	}
+
+/*
+$req = "INSERT INTO calendar VALUES ( '1', '1', 'Y', '1')";
+$res = $db->db_query($req);
+$req = "INSERT INTO calendar VALUES ( '2', '1', 'Y', '2')";
+$res = $db->db_query($req);
+$req = "INSERT INTO calendar VALUES ( '3', '2', 'N', '2')";
+$res = $db->db_query($req);
+$req = "INSERT INTO calendar VALUES ( '4', '3', 'Y', '2')";
+$res = $db->db_query($req);
+*/
+
+$req = "CREATE TABLE calaccess_users (";
+$req .= "id INT (11) UNSIGNED not null AUTO_INCREMENT,";
+$req .= "id_cal INT (11) UNSIGNED not null,";
+$req .= "id_user INT (11) UNSIGNED not null,";
+$req .= "bwrite enum('N','Y') DEFAULT 'N' NOT NULL,";
+$req .= "PRIMARY KEY (id)";
+$req .= ")";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>calaccess_users</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "select * from users";
+$res1 = $db->db_query($req);
+while( $arr = $db->db_fetch_array($res1))
+	{
+	$req = "insert into calendar (owner, actif, type) values ('".$arr[id]."', 'Y', '1')";
+	$res = $db->db_query($req);
+	}
+
+$req = "select * from groups";
+$res1 = $db->db_query($req);
+while( $arr = $db->db_fetch_array($res1))
+	{
+	if( $arr[id] == 2)
+		$req = "insert into calendar (owner, actif, type) values ('".$arr[id]."', 'N', '2')";
+	else
+		$req = "insert into calendar (owner, actif, type) values ('".$arr[id]."', 'Y', '2')";
+	$res = $db->db_query($req);
+	}
+return $ret;
+}
+
+?>
--- version.php
+++ version.php
@@ -1,3 +1,65 @@
+<?php
+$CurrentVersion = "2.3";
+
+function getVersion()
+{
+	$filename = "config.php";
+
+	$file = @fopen($filename, "r");
+	$txt = fread($file, filesize($filename));
+	fclose($file);
+	$reg = "/babVersion\s*=\s*\"([^ ]*)\"/s";
+	$res = preg_match($reg, $txt, $match);
+
+	return $match[1];
+}
+
+function putVersion($version)
+{
+	$filename = "config.php";
+
+	$file = @fopen($filename, "r");
+	$txt = fread($file, filesize($filename));
+	fclose($file);
+	$reg = "/babVersion\s*=\s*\"([^ ]*)\"/s";
+	$res = preg_match($reg, $txt, $match);
+
+	$reg = "/babVersion\s*=\s*\"".$match[1]."\"/s";
+	$out = preg_replace($reg, "babVersion = \"".$version."\"", $txt);
+	$file = fopen($filename, "w");
+	fputs($file, $out);
+	fclose($file);
+	return $match[1];
+}
+
+/* main */
+if( !isset($idx))
+	$idx = "version";
+
+$str = "";
+switch($idx)
+	{
+	case "upgrade";
+		$oldversion = getVersion();
+		if( $oldversion == $CurrentVersion)
+		{
+			$str = "Version ". $CurrentVersion . "<br>";
+			$str .= babTranslate("You site is already up to date");
+		}
+		$oldversion = strtr($oldversion, ".", "-");
+		include "upgrade".$oldversion."to".strtr($CurrentVersion, ".", "-");
+		$str = upgrade();
+		if( empty($str))
+			putVersion(CurrentVersion);
+		break;
+
+	case "version":
+	default:
+		$str = "Version ". $CurrentVersion;
+		break;
+	}
+?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <HTML>
 <HEAD>
@@ -9,10 +71,9 @@
 </HEAD>
 <BODY BGCOLOR="#FFFFFF">
 <center><H1>Ovidentia</H1>
-<?php
-echo "Version 2.3";
-?>
+<?php echo $str; ?>
 <br>
 <P class="copyright">&copy; 2001, <a href="http://www.cantico.fr/">CANTICO</a> All rights reserved.</P></center>
 </BODY>
 </HTML>
+
