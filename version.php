696f8b177450b95a418c0b14fcba46a09e167ec5
nouaya <nouaya>
2001-07-05 16:03:01 +0000

    add sitename

--- version.php
+++ version.php
@@ -71,7 +71,10 @@ switch($idx)
 </HEAD>
 <BODY BGCOLOR="#FFFFFF">
 <center><H1>Ovidentia</H1>
-<?php echo $str; ?>
+<?php
+echo $babSiteName . "<br>";
+echo $str;
+?>
 <br>
 <P class="copyright">&copy; 2001, <a href="http://www.cantico.fr/">CANTICO</a> All rights reserved.</P></center>
 </BODY>
