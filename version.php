e2cb7e68784314d834a3c0c4aa72713d006196d7
nouaya <nouaya>
2001-10-11 13:43:09 +0000

    Added update of language file

--- version.php
+++ version.php
@@ -13,9 +13,8 @@ function getVersion()
 	$file = @fopen($filename, "r");
 	$txt = fread($file, filesize($filename));
 	fclose($file);
-	$reg = "/babVersion\s*=\s*\"([^\"]*)\"/s";
-	$res = preg_match($reg, $txt, $match);
-
+	$reg = "babVersion[[:space:]]*=[[:space:]]*\"([^\"]*)\"";
+	$res = ereg($reg, $txt, $match);
 	return $match[1];
 }
 
@@ -26,15 +25,48 @@ function putVersion($version)
 	$file = @fopen($filename, "r");
 	$txt = fread($file, filesize($filename));
 	fclose($file);
-	$reg = "/babVersion\s*=\s*\"([^\"]*)\"/s";
-	$res = preg_match($reg, $txt, $match);
+	$reg = "babVersion[[:space:]]*=[[:space:]]*\"([^\"]*)\"";
+	$res = ereg($reg, $txt, $match);
 
-	$reg = "/babVersion\s*=\s*\"".$match[1]."\"/s";
-	$out = preg_replace($reg, "babVersion = \"".$version."\"", $txt);
+	$reg = "babVersion[[:space:]]*=[[:space:]]*\"".$match[1]."\"";
+	$out = ereg_replace($reg, "babVersion = \"".$version."\"", $txt);
 	$file = fopen($filename, "w");
 	fputs($file, $out);
 	fclose($file);
 	return $match[1];
+}
+
+function echoLang($path)
+{
+	$arr = array();
+	if( empty($GLOBALS[babLanguage]))
+		return;
+	$handle = opendir($path); 
+	while (false !== ($filename = readdir($handle)))
+		{ 
+		if ($filename != "." && $filename != "..")
+			{
+			if (is_dir($path.$filename))
+				{
+					$arr = array_merge($arr, echoLang($path.$filename."/"));
+				}
+			else
+				{
+				$file = @fopen($path.$filename, "r");
+				$txt = fread($file, filesize($path.$filename));
+				fclose($file);
+				$reg = "/babTranslate[[:space:]]*\([[:space:]]*\"([^\"]*)/s";
+				preg_match_all($reg, $txt, $m1);
+				for ($i = 0; $i < count($m1[1]); $i++ )
+					{
+					if( !empty($m1[1][$i]) && !in_array($m1[1][$i], $arr) )
+						$arr[] = $m1[1][$i];
+					}
+				}
+			} 
+		}
+	closedir($handle);
+	return $arr;
 }
 
 /* main */
@@ -57,6 +89,37 @@ switch($idx)
 		if( empty($str))
 			putVersion($CurrentVersion);
 		break;
+
+	case "lang":
+		$tab = array_unique(echoLang($GLOBALS[babInstallPath]));
+		$filename = "lang/lang-".$GLOBALS[babLanguage].".xml";
+		if( !file_exists($filename))
+			{
+			$txt = "";
+			}
+		else
+			{
+			$file = @fopen($filename, "r");
+			$txt = fread($file, filesize($filename));
+			fclose($file);
+			}
+		$append = "";
+		for( $i = 0; $i < count($tab); $i++)
+			{
+			$reg = "<string[[:space:]]*id=\"$tab[$i]\">([^<]*)<\/string>";
+			if( !empty($tab[$i]) && !ereg($reg, $txt, $m))
+				{
+				$append .= "<string id=\"$tab[$i]\">".$tab[$i]."</string>"."\r\n";
+				}
+			}
+		$reg = "/<".$GLOBALS[babLanguage].">(.*)<\/".$GLOBALS[babLanguage].">/s";
+		preg_match($reg, $txt, $m);
+		$file = fopen($filename, "w");
+		fputs($file, "<".$GLOBALS[babLanguage].">".$m[1].$append."</".$GLOBALS[babLanguage].">");
+		fclose($file);
+		$str = babTranslate("You language file has been updated") ."( ".$filename." )";
+		break;
+
 
 	case "version":
 	default:
