c5f053c5b9fd9dc03b4aa03be28192e5c31370f0
nouaya <nouaya>
2001-10-15 14:31:22 +0000

    not used

--- utilit/date-us.php
+++
@@ -1,7 +0,0 @@
-<?php
-$babMonths = array(1=>"January", "February", "March", "April",
-                        "May", "June", "July", "August",
-                        "September", "October", "November", "December");
-
-$babDays = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-?>
