46e6904a165388da84810433cd21a98c8083befa
nouaya <nouaya>
2001-11-26 17:13:31 +0000

    First add

---
+++ example/config.php
@@ -0,0 +1,16 @@
+<?php
+$babDBHost = "localhost";
+$babDBLogin = "root";
+$babDBPasswd = "";
+$babDBName ="dev";
+$babInstallPath = "../";
+$babSkin = "ovidentia";
+$babStyle = "ovidentia.css";
+$babSlogan = "Bab: un portail pour les entreprises";
+$babSiteName = "Ovidentia";
+$babUrl = "http://localhost/bab31/example/";
+$babAdminEmail = "info@cantico.fr";
+$babLanguage = "fr";
+$babMaxRows = 10; // threads.php
+$babVersion = "3.1";
+?>
