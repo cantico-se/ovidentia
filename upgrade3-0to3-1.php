c80efa9daff66e9051e3a52f429377b489ee5698
nouaya <nouaya>
2001-12-20 22:58:13 +0000

    no message

--- upgrade3-0to3-1.php
+++ upgrade3-0to3-1.php
@@ -10,6 +10,9 @@
 	- Open/Close section
 	- Search
 	- Add new articles, comments, posts to 
+	- Upload/Download
+	- Search contacts
+	- Search in Files
 ************************************************************************/
 include "config.php";
 
@@ -138,6 +141,86 @@ if( !$res)
 	$ret = "Creation of <b>sections_states</b> table failed !<br>";
 	return $ret;
 	}
+
+$req = "ALTER TABLE groups ADD gstorage ENUM('N','Y') NOT NULL, ADD ustorage ENUM('N','Y') NOT NULL;";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>groups</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "CREATE TABLE files (";
+$req .= " id int(11) unsigned NOT NULL auto_increment,";
+$req .= " name varchar(255) NOT NULL default '',";
+$req .= " description tinytext NOT NULL,";
+$req .= " keywords tinytext NOT NULL,";
+$req .= " path tinytext NOT NULL,";
+$req .= " id_owner int(11) unsigned NOT NULL default '0',";
+$req .= " bgroup enum('N','Y') NOT NULL default 'N',";
+$req .= " link int(11) unsigned NOT NULL default '0',";
+$req .= " readonly enum('N','Y') NOT NULL default 'N',";
+$req .= " state char(1) NOT NULL default '',";
+$req .= " created datetime default NULL,";
+$req .= " author int(11) unsigned NOT NULL default '0',";
+$req .= " modified datetime default NULL,";
+$req .= " modifiedby int(11) unsigned NOT NULL default '0',";
+$req .= " confirmed enum('N','Y') NOT NULL default 'N',";
+$req .= " PRIMARY KEY  (id)";
+$req .= " );";
+
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>files</b> table failed !<br>";
+	return $ret;
+	}
+
+$req .= "CREATE TABLE mime_types (";
+$req .= " ext VARCHAR(10) NOT NULL, ";
+$req .= " mimetype TINYTEXT NOT NULL,";
+$req .= " PRIMARY KEY (ext)";
+$req .= " ); ";
+
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>mime_types</b> table failed !<br>";
+	return $ret;
+	}
+
+$db->db_query("INSERT INTO mime_types VALUES ('ai', 'application/postscript')";
+$db->db_query("INSERT INTO mime_types VALUES ('asc', 'text/plain')";
+$db->db_query("INSERT INTO mime_types VALUES ('au', 'audio/basic')";
+$db->db_query("INSERT INTO mime_types VALUES ('avi', 'video/x-msvideo')";
+$db->db_query("INSERT INTO mime_types VALUES ('bin', 'application/octet-stream')";
+$db->db_query("INSERT INTO mime_types VALUES ('bmp', 'image/bmp')";
+$db->db_query("INSERT INTO mime_types VALUES ('class', 'application/octet-stream')";
+$db->db_query("INSERT INTO mime_types VALUES ('css', 'text/css')";
+$db->db_query("INSERT INTO mime_types VALUES ('doc', 'application/msword')";
+$db->db_query("INSERT INTO mime_types VALUES ('dvi', 'application/x-dvi')";
+$db->db_query("INSERT INTO mime_types VALUES ('exe', 'application/octet-stream')";
+$db->db_query("INSERT INTO mime_types VALUES ('gif', 'image/gif')";
+$db->db_query("INSERT INTO mime_types VALUES ('htm', 'text/html')";
+$db->db_query("INSERT INTO mime_types VALUES ('html', 'text/html')";
+$db->db_query("INSERT INTO mime_types VALUES ('jpe', 'image/jpeg')";
+$db->db_query("INSERT INTO mime_types VALUES ('jpeg', 'image/jpeg')";
+$db->db_query("INSERT INTO mime_types VALUES ('jpg', 'image/jpeg')";
+$db->db_query("INSERT INTO mime_types VALUES ('js', 'application/x-javascript')";
+$db->db_query("INSERT INTO mime_types VALUES ('mid', 'audio/midi')";
+$db->db_query("INSERT INTO mime_types VALUES ('midi', 'audio/midi')";
+$db->db_query("INSERT INTO mime_types VALUES ('mp3', 'audio/mpeg')";
+$db->db_query("INSERT INTO mime_types VALUES ('mpeg', 'video/mpeg')";
+$db->db_query("INSERT INTO mime_types VALUES ('png', 'image/png');
+$db->db_query("INSERT INTO mime_types VALUES ('ppt', 'application/vnd.ms-powerpoint')";
+$db->db_query("INSERT INTO mime_types VALUES ('ps', 'application/postscript')";
+$db->db_query("INSERT INTO mime_types VALUES ('rtf', 'text/rtf')";
+$db->db_query("INSERT INTO mime_types VALUES ('tar', 'application/x-tar')";
+$db->db_query("INSERT INTO mime_types VALUES ('txt', 'text/plain')";
+$db->db_query("INSERT INTO mime_types VALUES ('wav', 'audio/x-wav')";
+$db->db_query("INSERT INTO mime_types VALUES ('xls', 'application/vnd.ms-excel')";
+$db->db_query("INSERT INTO mime_types VALUES ('xml', 'text/xml')";
+$db->db_query("INSERT INTO mime_types VALUES ('zip', 'application/zip')";
 
 return $ret;
 }
