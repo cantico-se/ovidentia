bc04588fc244f98f2f7354c499d74a0c17e8fd3e
nicbid <nicbid>
2002-01-23 19:07:02 +0000

    Added comments

--- example/config.php
+++ example/config.php
@@ -1,21 +1,27 @@
 <?php
-$babDBHost = "localhost";
-$babDBLogin = "root";
-$babDBPasswd = "";
-$babDBName ="dev";
-$babInstallPath = "../";
-$babSkin = "ovidentia";
-$babStyle = "ovidentia.css";
-$babSlogan = "Ovidentia : un portail pour les entreprises";
-$babSiteName = "Ovidentia";
-$babUrl = "http://localhost/bab31/example/";
-$babAdminEmail = "community@ovidentia.org";
-$babLanguage = "fr";
+/***********************************************************************
+* Ovidentia                                                            *
+************************************************************************
+* Copyright (c) 2001, CANTICO ( http://www.cantico.fr )                *
+* Ovidentia web site ( http://www.ovidentia.org )                      *
+***********************************************************************/
+$babDBHost = "localhost"; /* MySql database server */
+$babDBLogin = "root"; /* MySql database login */
+$babDBPasswd = ""; /* MySql database password */
+$babDBName ="ovidentia"; /* MySql database name */
+$babInstallPath = "ovidentia3-1"; /* path to ovidentia distribution */
+$babSkin = "ovidentia"; /* skin to use */
+$babStyle = "ovidentia.css"; /* css file to use */
+$babSlogan = "Ovidentia: enterprise portal"; /* your slogan */
+$babSiteName = "Ovidentia"; /* your site name */
+$babUrl = "http://localhost/bab31/example/"; /* url to access to your site */
+$babAdminEmail = "admin@your-domain.com"; /* email address used to send emails ( registration, ... ) */
+$babLanguage = "fr"; /* default language */
 $babMaxRows = 10; 
-$babVersion = "3.1";
-$babMaxFileSize = 1000000;
-$babMaxUserSize = 2000000;
-$babMaxGroupSize = 5000000;
-$babMaxTotalSize = 100000000;
-$babUploadPath = "C:\\easyphp\\www\\tmp";
+$babVersion = "3.1"; /* current version */
+$babMaxFileSize = 1000000; /* Max size ( bytes ) file allowed*/
+$babMaxUserSize = 2000000; /* Capacity storage ( bytes ) allowed by user*/
+$babMaxGroupSize = 5000000; /* Capacity storage ( bytes ) allowed by group */
+$babMaxTotalSize = 100000000; /* Capacity storage ( bytes ) allowed for a site */
+$babUploadPath = "/uploads-directory"; /* where to upload files */
 ?>
