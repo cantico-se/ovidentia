2c83ad1d839470ec8faf8d4a7a09732d0d886cef
nouaya <nouaya>
2002-03-08 01:40:48 +0000

    Minor change

--- upgrade3-2to3-3.php
+++ upgrade3-2to3-3.php
@@ -6,12 +6,16 @@
  ***********************************************************************/
 /* upgrade from 3.2 to 3.3 */
 /************************************************************************
+- support of addons
+- Addded prefered style sheet
+- Added $ARTICLE(topic, article)
+- Added $FAQ(faq, question)
 ************************************************************************/
 function upgrade()
 {
 $ret = "";
 $db = $GLOBALS['babDB'];
-
+/*
 $res = $db->db_query("show tables");
 if( !$res)
 	{
@@ -22,6 +26,15 @@ if( !$res)
 while( $arr = $db->db_fetch_array($res))
 	{
 	$db->db_query("ALTER TABLE ".$arr[0]." RENAME bab_".$arr[0]);
+	}
+*/
+
+$req = "ALTER TABLE users ADD style TEXT NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>users</b> table failed !<br>";
+	return $ret;
 	}
 
 return $ret;
