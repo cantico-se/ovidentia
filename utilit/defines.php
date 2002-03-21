6bd9aa321b91afc27ed209b02f1bef23da449f11
nouaya <nouaya>
2002-03-21 20:57:22 +0000

    First add

---
+++ utilit/defines.php
@@ -0,0 +1,52 @@
+<?php
+/************************************************************************
+ * Ovidentia                                                            *
+ ************************************************************************
+ * Copyright (c) 2001, CANTICO ( http://www.cantico.fr )                *
+ ***********************************************************************/
+define("BAB_ARTICLES_TBL", "bab_ARTICLES");
+define("BAB_CAL_EVENTS_TBL", "bab_CAL_EVENTS");
+define("BAB_CALACCESS_USERS_TBL", "bab_calaccess_users");
+define("BAB_CALENDAR_TBL", "bab_calendar");
+define("BAB_CALOPTIONS_TBL", "bab_cal_events");
+define("BAB_CATEGORIESCAL_TBL", "bab_calaccess_users");
+define("BAB_COMMENTS_TBL", "bab_comments");
+define("BAB_CONTACTS_TBL", "bab_contacts");
+define("BAB_FAQCAT_TBL", "bab_faqcat");
+define("BAB_FAQCAT_GROUPS_TBL", "bab_faqcat_groups");
+define("BAB_FAQQR_TBL", "bab_faqqr");
+define("BAB_FILES_TBL", "bab_files");
+define("BAB_FORUMS_TBL", "bab_forums");
+define("BAB_FORUMSPOST_GROUPS_TBL", "bab_forumspost_groups");
+define("BAB_FORUMSREPLY_GROUPS_TBL", "bab_forumsreply_groups");
+define("BAB_FORUMSVIEW_GROUPS_TBL", "bab_forumsview_groups");
+define("BAB_GROUPS_TBL", "bab_groups");
+define("BAB_HOMEPAGES_TBL", "bab_homepages");
+define("BAB_MAIL_ACCOUNTS_TBL", "bab_mail_accounts");
+define("BAB_MAIL_DOMAINS_TBL", "bab_mail_domains");
+define("BAB_MAIL_SIGNATURES_TBL", "bab_mail_signatures");
+define("BAB_MIME_TYPES_TBL", "bab_mime_types");
+define("BAB_NOTES_TBL", "bab_notes");
+define("BAB_POSTS_TBL", "bab_posts");
+define("BAB_PRIVATE_SECTIONS_TBL", "bab_private_sections");
+define("BAB_RESOURCESCAL_TBL", "bab_resourcescal");
+define("BAB_SECTIONS_TBL", "bab_sections");
+define("BAB_SECTIONS_GROUPS_TBL", "bab_sections_groups");
+define("BAB_SECTIONS_ORDER_TBL", "bab_sections_order");
+define("BAB_SECTIONS_STATES_TBL", "bab_sections_states");
+define("BAB_SITES_TBL", "bab_sites");
+define("BAB_THREADS_TBL", "bab_threads");
+define("BAB_TOPICS_TBL", "bab_topics");
+define("BAB_TOPICS_CATEGORIES_TBL", "bab_topics_categories");
+define("BAB_TOPICSCOM_GROUPS_TBL", "bab_topicscom_groups");
+define("BAB_TOPICSSUB_GROUPS_TBL", "bab_topicssub_groups");
+define("BAB_TOPICSVIEW_GROUPS_TBL", "bab_topicsview_groups");
+define("BAB_USERS_TBL", "bab_users");
+define("BAB_USERS_GROUPS_TBL", "bab_users_groups");
+define("BAB_USERS_LOG_TBL", "bab_users_log");
+define("BAB_VACATIONS_TBL", "bab_vacations");
+define("BAB_VACATIONS_STATES_TBL", "bab_vacations_states");
+define("BAB_VACATIONS_TYPES_TBL", "bab_vacations_types");
+define("BAB_VACATIONSMAN_GROUPS_TBL", "bab_vacationsman_groups");
+define("BAB_VACATIONSVIEW_GROUPS_TBL", "bab_vacationsview_groups");
+?>
