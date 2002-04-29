44ccadd71c49e225243c7db5bba06e5a1d5718bd
nouaya <nouaya>
2002-04-29 20:09:30 +0000

    Minor change

--- distrib/config.php
+++ distrib/config.php
@@ -18,10 +18,11 @@ $babUrl = "http://yourdomain/"; /* url to access to your site */
 $babAdminEmail = "admin@your-domain.com"; /* email address used to send emails ( registration, ... ) */
 $babLanguage = "en"; /* default language */
 $babMaxRows = 10; 
-$babVersion = "3.2"; /* current version */
+$babVersion = "3.3"; /* current version */
 $babMaxFileSize = 1000000; /* Max size ( bytes ) file allowed*/
 $babMaxUserSize = 2000000; /* Capacity storage ( bytes ) allowed by user*/
 $babMaxGroupSize = 5000000; /* Capacity storage ( bytes ) allowed by group */
 $babMaxTotalSize = 100000000; /* Capacity storage ( bytes ) allowed for a site */
 $babUploadPath = "/uploads-directory"; /* where to upload files ( c:\\path-to\\upload-directory for Windows )*/
+$babFileNameTranslation = array() /* translation characters for files names. Example array( " " => "_", "/" => "_"); */
 ?>
