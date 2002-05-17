54183f0256863abfd353b71220684940bf53964d
nouaya <nouaya>
2002-05-17 15:29:59 +0000

    First add

---
+++ upgrade.php
@@ -0,0 +1,303 @@
+<?php
+/************************************************************************
+ * Ovidentia                                                            *
+ ************************************************************************
+ * Copyright (c) 2001, CANTICO ( http://www.cantico.fr )                *
+ ***********************************************************************/
+/************************************************************************
+************************************************************************/
+function upgrade310to320()
+{
+$ret = "";
+$db = $GLOBALS['babDB'];
+
+$req = "ALTER TABLE files ADD hits INT(11) UNSIGNED NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>files</b> table failed !<br>";
+	return $ret;
+	}
+	
+$req = "ALTER TABLE sections ADD enabled ENUM('Y','N') DEFAULT 'Y' NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>sections</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE private_sections ADD enabled ENUM('Y','N') DEFAULT 'Y' NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>private_sections</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE topics ADD id_cat INT(11) UNSIGNED NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>topics</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "select id, private from sections_order";
+$res = $db->db_query($req);
+
+$req = "ALTER TABLE sections_order CHANGE private type SMALLINT(2) UNSIGNED NOT NULL ";
+$res1 = $db->db_query($req);
+if( !$res1)
+	{
+	$ret = "Alteration of <b>sections_order</b> table failed !<br>";
+	return $ret;
+	}
+
+while( $arr = $db->db_fetch_array($res))
+	{
+	if( $arr['private'] == "Y")
+		$db->db_query("update sections_order set type='1' where id='".$arr['id']."'");
+	else
+		$db->db_query("update sections_order set type='2' where id='".$arr['id']."'");
+	}
+
+$req = "select id, private from sections_states";
+$res = $db->db_query($req);
+
+$req = "ALTER TABLE sections_states CHANGE private type SMALLINT(2) UNSIGNED NOT NULL ";
+$res1 = $db->db_query($req);
+if( !$res1)
+	{
+	$ret = "Alteration of <b>sections_states</b> table failed !<br>";
+	return $ret;
+	}
+
+while( $arr = $db->db_fetch_array($res))
+	{
+	if( $arr['private'] == "Y")
+		$db->db_query("update sections_states set type='1' where id='".$arr['id']."'");
+	else
+		$db->db_query("update sections_states set type='2' where id='".$arr['id']."'");
+	}
+
+$req = "CREATE TABLE topics_categories (";
+$req .= "id int(11) unsigned NOT NULL auto_increment, ";
+$req .= "title varchar(60), ";
+$req .= "description varchar(200), ";
+$req .= "enabled enum('Y','N') DEFAULT 'Y' NOT NULL, ";
+$req .= "PRIMARY KEY (id) ";
+$req .= ")";
+
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>topics_categories</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "INSERT INTO topics_categories VALUES ('1', 'Default category', 'Default category', 'Y')";
+$res = $db->db_query($req);
+$id = $db->db_insert_id();
+
+$req = "update topics set id_cat='1'";
+$res = $db->db_query($req);
+
+$req = "select max(ordering) from sections_order where position='0'";
+$res = $db->db_query($req);
+$arr = $db->db_fetch_array($res);
+$req = "insert into sections_order (id_section, position, type, ordering) VALUES ('" .$id. "', '0', '3', '" . ($arr[0]+1). "')";
+$db->db_query($req);
+
+return $ret;
+}
+
+
+function upgrade320to330()
+{
+$ret = "";
+$db = $GLOBALS['babDB'];
+
+$res = $db->db_query("show tables");
+if( !$res)
+	{
+	$ret = "Alteration of <b>tables</b>failed !<br>";
+	return $ret;
+	}
+
+while( $arr = $db->db_fetch_array($res))
+	{
+	$db->db_query("ALTER TABLE ".$arr[0]." RENAME bab_".$arr[0]);
+	}
+
+
+$req = "ALTER TABLE ".BAB_USERS_TBL." ADD style TEXT NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_SITES_TBL." ADD registration ENUM('Y','N') NOT NULL, ADD email_confirm ENUM('Y','N') NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_SITES_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_TBL." ADD lastlog DATETIME NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_TBL." ADD datelog DATETIME NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$res = $db->db_query("select id from ".BAB_USERS_TBL);
+while( $arr = $db->db_fetch_array($res))
+	{
+	$res2 = $db->db_query("select lastlog, datelog from ".BAB_USERS_LOG_TBL." where id_user='".$arr['id']."'");
+	if( $res2 && $db->db_num_rows($res2) > 0)
+		{
+		$rr = $db->db_fetch_array($res2);
+		$db->db_query("update ".BAB_USERS_TBL." set lastlog='".$rr['lastlog']."', datelog='".$rr['datelog']."' where id='".$arr['id']."'");
+		}
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_LOG_TBL." DROP lastlog";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_LOG_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_LOG_TBL." DROP datelog";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_LOG_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_LOG_TBL." DROP islogged";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_LOG_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_LOG_TBL." ADD sessid tinytext NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_LOG_TBL." CHANGE dateact dateact TIMESTAMP(14) DEFAULT '0000-00-00 00:00:00' NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_LOG_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_LOG_TBL." ADD remote_addr varchar(255) NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_USERS_LOG_TBL." ADD forwarded_for varchar(255) NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_USERS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_TOPICS_TBL." ADD mod_com ENUM('Y','N') DEFAULT 'Y' NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_TOPICS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "ALTER TABLE ".BAB_ARTICLES_TBL." ADD archive ENUM('N','Y') NOT NULL";
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Alteration of <b>".BAB_ARTICLES_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+$req = "CREATE TABLE ".BAB_ADDONS_TBL." (";
+$req .= "id int(11) unsigned NOT NULL auto_increment,";
+$req .= "title varchar(255) NOT NULL default '',";
+$req .= "enabled enum('Y','N') NOT NULL default 'Y',";
+$req .= "PRIMARY KEY  (id)";
+$req .= ");";
+
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>".BAB_ADDONS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+
+
+$req = "CREATE TABLE ".BAB_ADDONS_GROUPS_TBL." (";
+$req .= "id int(11) unsigned NOT NULL auto_increment,";
+$req .= "id_object int(11) unsigned DEFAULT '0' NOT NULL,";
+$req .= "id_group int(11) unsigned DEFAULT '0' NOT NULL,";
+$req .= "PRIMARY KEY (id)";
+$req .= ");";
+
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>".BAB_ADDONS_GROUPS_TBL."</b> table failed !<br>";
+	return $ret;
+	}
+return $ret;
+}
+
+function upgrade330to331()
+{
+$ret = "";
+$db = $GLOBALS['babDB'];
+
+$req = "CREATE TABLE ".BAB_INI_TBL." (";
+$req .= "foption char(255) NOT NULL default '',";
+$req .= "fvalue char(255) NOT NULL default '',";
+$req .= "UNIQUE KEY foption (foption)";
+$req .= ");";
+
+$res = $db->db_query($req);
+if( !$res)
+	{
+	$ret = "Creation of <b>".BAB_INI_TBL."</b> table failed !<br>";
+	return $false;
+	}
+
+$ar = explode(".", $GLOBALS['babVersion']);
+$db->db_query("INSERT INTO ".BAB_INI_TBL." VALUES ('ver_major', '".$ar[0]."')");
+$db->db_query("INSERT INTO ".BAB_INI_TBL." VALUES ('ver_minor', '".$ar[1]."')");
+$db->db_query("INSERT INTO ".BAB_INI_TBL." VALUES ('ver_build', '0')");
+}
+?>
