7767aca50b03b89ce8f099c10c3a24aa9760671f
nouaya <nouaya>
2002-05-27 21:39:08 +0000

    no message

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -394,7 +394,7 @@ CREATE TABLE bab_users (
    PRIMARY KEY (id)
 );
 
-INSERT INTO bab_users VALUES ( '1', 'admin@admin.bab', 'Administrator', '', '200ceb26807d6bf99fd6f4f0d1ca54d4', 'admin@admin.bab', '2001-04-03 00:00:00', '22975d8a5ed1b91445f6c55ac121505b', '0', '', '0da8f2a37b9e7926e08196a6bd1baa29', '1', '0', '', '', '', '', '');
+INSERT INTO bab_users VALUES ( '1', 'admin@admin.bab', 'Administrator', '', '200ceb26807d6bf99fd6f4f0d1ca54d4', 'admin@admin.bab', '2001-04-03 00:00:00', '22975d8a5ed1b91445f6c55ac121505b', '1', '', '0da8f2a37b9e7926e08196a6bd1baa29', '1', '0', '', '', '', '', '');
 
 # --------------------------------------------------------
 #
