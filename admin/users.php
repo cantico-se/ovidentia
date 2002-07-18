fd4b9216568eb8728706ae53cd6b27259dfe6624
nouaya <nouaya>
2002-07-18 08:27:44 +0000

    Added browse list users

--- admin/users.php
+++ admin/users.php
@@ -6,6 +6,126 @@
  ***********************************************************************/
 include $babInstallPath."admin/register.php";
 
+function browseUsers($pos, $cb)
+	{
+	global $babBody;
+	class temp
+		{
+		var $fullname;
+		var $urlname;
+		var $url;
+		var $email;
+		var $status;
+				
+		var $fullnameval;
+		var $emailval;
+
+		var $arr = array();
+		var $db;
+		var $count;
+		var $res;
+
+		var $pos;
+
+		var $userid;
+
+		var $nickname;
+
+		function temp($pos, $cb)
+			{
+			$this->allname = bab_translate("All");
+			$this->nickname = bab_translate("Nickname");
+			$this->db = $GLOBALS['babDB'];
+			$this->cb = $cb;
+
+			if( $pos[0] == "-" )
+				{
+				$this->pos = $pos[1];
+				$this->ord = $pos[0];
+				$req = "select * from ".BAB_USERS_TBL." where lastname like '".$this->pos."%' order by lastname, firstname asc";
+				$this->fullname = bab_translate("Lastname"). " " . bab_translate("Firstname");
+				$this->fullnameurl = $GLOBALS['babUrlScript']."?tg=users&idx=brow&pos=".$this->pos."&cb=".$this->cb;
+				}
+			else
+				{
+				$this->pos = $pos;
+				$this->ord = "";
+				$req = "select * from ".BAB_USERS_TBL." where firstname like '".$this->pos."%' order by firstname, lastname asc";
+				$this->fullname = bab_translate("Firstname"). " " . bab_translate("Lastname");
+				$this->fullnameurl = $GLOBALS['babUrlScript']."?tg=users&idx=brow&pos=-".$this->pos."&cb=".$this->cb;
+				}
+			$this->res = $this->db->db_query($req);
+			$this->count = $this->db->db_num_rows($this->res);
+
+			if( empty($this->pos))
+				$this->allselected = 1;
+			else
+				$this->allselected = 0;
+			$this->allurl = $GLOBALS['babUrlScript']."?tg=users&idx=brow&pos=&cb=".$this->cb;
+			}
+
+		function getnext()
+			{
+			static $i = 0;
+			if( $i < $this->count)
+				{
+				$this->arr = $this->db->db_fetch_array($this->res);
+				$this->url = $GLOBALS['babUrlScript']."?tg=user&idx=Modify&item=".$this->arr['id']."&pos=".$this->ord.$this->pos."&cb=".$this->cb;
+				$this->firstlast = bab_composeUserName($this->arr['firstname'],$this->arr['lastname']);
+				$this->firstlast = str_replace("'", "\'", $this->firstlast);
+				$this->firstlast = str_replace('"', "'+String.fromCharCode(34)+'",$this->firstlast);
+				if( $this->ord == "-" )
+					$this->urlname = bab_composeUserName($this->arr['lastname'],$this->arr['firstname']);
+				else
+					$this->urlname = bab_composeUserName($this->arr['firstname'],$this->arr['lastname']);
+				$this->urlname .= "(".$this->arr['id'].")";
+				$this->userid = $this->arr['id'];
+				$i++;
+				return true;
+				}
+			else
+				return false;
+
+			}
+
+		function getnextselect()
+			{
+			global $BAB_SESS_USERID;
+			static $k = 0;
+			static $t = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			if( $k < 26)
+				{
+				$this->selectname = substr($t, $k, 1);
+				$this->selecturl = $GLOBALS['babUrlScript']."?tg=users&idx=brow&pos=".$this->ord.$this->selectname."&cb=".$this->cb;
+
+				if( $this->pos == $this->selectname)
+					$this->selected = 1;
+				else 
+					{
+					if( $this->ord == "-" )
+						$req = "select * from ".BAB_USERS_TBL." where lastname like '".$this->selectname."%'";
+					else
+						$req = "select * from ".BAB_USERS_TBL." where firstname like '".$this->selectname."%'";
+					$res = $this->db->db_query($req);
+					if( $this->db->db_num_rows($res) > 0 )
+						$this->selected = 0;
+					else
+						$this->selected = 1;
+					}
+				$k++;
+				return true;
+				}
+			else
+				return false;
+
+			}
+		}
+
+	$temp = new temp($pos, $cb);
+	echo bab_printTemplate($temp, "users.html", "browseusers");
+	}
+
+
 function listUsers($pos, $grp)
 	{
 	global $babBody;
@@ -259,6 +379,10 @@ if( isset($Updateg))
 
 switch($idx)
 	{	
+	case "brow":
+		browseUsers($pos, $cb);
+		exit;
+		break;
 	case "Create":
 		$babBody->title = bab_translate("Create a user");
 		userCreate($firstname, $lastname, $nickname, $email);
