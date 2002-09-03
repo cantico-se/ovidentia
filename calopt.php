caaa1b8c320e818920693252436ad7a60cd35430
nouaya <nouaya>
2002-09-03 13:21:00 +0000

    Added possibility to give calendar access to a list of users

--- calopt.php
+++ calopt.php
@@ -4,6 +4,168 @@
  ************************************************************************
  * Copyright (c) 2001, CANTICO ( http://www.cantico.fr )                *
  ***********************************************************************/
+function listUsersUnload()
+	{
+	class temp
+		{
+		var $babCss;
+		var $message;
+		var $close;
+
+		function temp()
+			{
+			$this->message = bab_translate("Calendar access has been updated");
+			$this->close = bab_translate("Close");
+			}
+		}
+
+	$temp = new temp();
+	echo bab_printTemplate($temp,"calopt.html", "listunload");
+	}
+
+function browseUsers($pos, $cb, $idcal)
+	{
+	global $babBody;
+	class temp
+		{
+		var $fullname;
+		var $urlname;
+		var $url;
+		var $email;
+		var $status;
+				
+		var $fullnameval;
+		var $emailval;
+
+		var $arr = array();
+		var $db;
+		var $count;
+		var $res;
+
+		var $pos;
+
+		var $userid;
+
+		var $nickname;
+
+		function temp($pos, $cb, $idcal)
+			{
+			$this->allname = bab_translate("All");
+			$this->nickname = bab_translate("Nickname");
+			$this->vaccname0 = bab_translate("Consultation");
+			$this->vaccname1 = bab_translate("Creation and modification");
+			$this->vaccname2 = bab_translate("Total access");
+			$this->useraccess = bab_translate("Access");
+			$this->addusers = bab_translate("Update access");
+			$this->db = $GLOBALS['babDB'];
+			$this->cb = $cb;
+			$this->calid = $idcal;
+			if( !bab_isUserAdministrator())
+				{
+				$req = "select ".BAB_GROUPS_TBL.".id from ".BAB_GROUPS_TBL." join ".BAB_USERS_GROUPS_TBL." where id_object='".$GLOBALS['BAB_SESS_USERID']."' and ".BAB_GROUPS_TBL.".id=".BAB_USERS_GROUPS_TBL.".id_group";
+				$resgroups = $this->db->db_query($req);
+
+				$reqa = "select distinct ".BAB_USERS_TBL.".id, ".BAB_USERS_TBL.".firstname, ".BAB_USERS_TBL.".lastname, ".BAB_USERS_TBL.".nickname from ".BAB_USERS_TBL." join ".BAB_USERS_GROUPS_TBL." where is_confirmed ='1' and disabled='0'";
+				if( $this->db->db_num_rows($resgroups) > 0 )
+					{
+					$arr = $this->db->db_fetch_array($resgroups);
+					$reqa .= " and ( ".BAB_USERS_GROUPS_TBL.".id_group='".$arr['id']."'";
+					while($arr = $this->db->db_fetch_array($resgroups))
+						{
+						$reqa .= " or ".BAB_USERS_GROUPS_TBL.".id_group='".$arr['id']."'"; 
+						}
+					$reqa .= ") and ".BAB_USERS_GROUPS_TBL.".id_object=".BAB_USERS_TBL.".id";
+					}
+				}
+			else
+				$reqa = "select * from ".BAB_USERS_TBL." where is_confirmed ='1' and disabled='0'";
+
+			if( $pos[0] == "-" )
+				{
+				$this->pos = $pos[1];
+				$this->ord = $pos[0];
+				$reqa .= " and lastname like '".$this->pos."%' order by lastname, firstname asc";
+				$this->fullname = bab_translate("Lastname"). " " . bab_translate("Firstname");
+				$this->fullnameurl = $GLOBALS['babUrlScript']."?tg=calopt&idx=brow&pos=".$this->pos."&cb=".$this->cb;
+				}
+			else
+				{
+				$this->pos = $pos;
+				$this->ord = "";
+				$reqa .= " and firstname like '".$this->pos."%' order by firstname, lastname asc";
+				$this->fullname = bab_translate("Firstname"). " " . bab_translate("Lastname");
+				$this->fullnameurl = $GLOBALS['babUrlScript']."?tg=calopt&idx=brow&pos=-".$this->pos."&cb=".$this->cb;
+				}
+			$this->res = $this->db->db_query($reqa);
+			$this->count = $this->db->db_num_rows($this->res);
+
+			if( empty($this->pos))
+				$this->allselected = 1;
+			else
+				$this->allselected = 0;
+			$this->allurl = $GLOBALS['babUrlScript']."?tg=calopt&idx=brow&pos=&cb=".$this->cb;
+			}
+
+		function getnext()
+			{
+			static $i = 0;
+			if( $i < $this->count)
+				{
+				$this->arr = $this->db->db_fetch_array($this->res);
+				$this->url = $GLOBALS['babUrlScript']."?tg=user&idx=Modify&item=".$this->arr['id']."&pos=".$this->ord.$this->pos."&cb=".$this->cb;
+				$this->firstlast = bab_composeUserName($this->arr['firstname'],$this->arr['lastname']);
+				$this->firstlast = str_replace("'", "\'", $this->firstlast);
+				$this->firstlast = str_replace('"', "'+String.fromCharCode(34)+'",$this->firstlast);
+				if( $this->ord == "-" )
+					$this->urlname = bab_composeUserName($this->arr['lastname'],$this->arr['firstname']);
+				else
+					$this->urlname = bab_composeUserName($this->arr['firstname'],$this->arr['lastname']);
+				$this->userid = $this->arr['id'];
+				$this->nicknameval = $this->arr['nickname'];
+				$i++;
+				return true;
+				}
+			else
+				return false;
+
+			}
+
+		function getnextselect()
+			{
+			global $BAB_SESS_USERID;
+			static $k = 0;
+			static $t = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			if( $k < 26)
+				{
+				$this->selectname = substr($t, $k, 1);
+				$this->selecturl = $GLOBALS['babUrlScript']."?tg=calopt&idx=brow&pos=".$this->ord.$this->selectname."&cb=".$this->cb;
+
+				if( $this->pos == $this->selectname)
+					$this->selected = 1;
+				else 
+					{
+					if( $this->ord == "-" )
+						$req = "select * from ".BAB_USERS_TBL." where lastname like '".$this->selectname."%'";
+					else
+						$req = "select * from ".BAB_USERS_TBL." where firstname like '".$this->selectname."%'";
+					$res = $this->db->db_query($req);
+					if( $this->db->db_num_rows($res) > 0 )
+						$this->selected = 0;
+					else
+						$this->selected = 1;
+					}
+				$k++;
+				return true;
+				}
+			else
+				return false;
+
+			}
+		}
+
+	$temp = new temp($pos, $cb, $idcal);
+	echo bab_printTemplate($temp, "calopt.html", "browseusers");
+	}
 
 
 function accessCalendar($calid)
@@ -36,10 +198,9 @@ function accessCalendar($calid)
 			{
 			$this->db = $GLOBALS['babDB'];
 			$this->calid = $calid;
-			$this->userstxt = bab_translate("User");
+			$this->userstxt = bab_translate("Update");
 			$this->textinfo = bab_translate("Add user");
 			$this->addusers = bab_translate("Update access");
-			$this->useraccess = bab_translate("Access");
 			$this->fullname = bab_translate("Fullname");
 			$this->accessname = bab_translate("Access");
 			$this->delusers = bab_translate("Delete users");
@@ -49,7 +210,7 @@ function accessCalendar($calid)
 			$req = "select * from ".BAB_CALACCESS_USERS_TBL." where id_cal='".$calid."'";
 			$this->res = $this->db->db_query($req);
 			$this->count = $this->db->db_num_rows($this->res);
-			$this->usersbrowurl = $GLOBALS['babUrlScript']."?tg=lusers&idx=brow&cb=";
+			$this->usersbrowurl = $GLOBALS['babUrlScript']."?tg=calopt&idx=brow";
 			}
 
 		function getnext()
@@ -90,23 +251,26 @@ function accessCalendar($calid)
 	$babBody->babecho(	bab_printTemplate($temp,"calopt.html", "access"));
 }
 
-function addAccessUsers( $userid, $calid, $baccess)
+function addAccessUsers( $users, $calid, $baccess)
 {
 
 	$db = $GLOBALS['babDB'];
-	$req = "select * from ".BAB_CALACCESS_USERS_TBL." where id_cal='".$calid."' and id_user='".$userid."'";
+	for( $i=0; $i < sizeof($users); $i++)
+	{
+	$req = "select * from ".BAB_CALACCESS_USERS_TBL." where id_cal='".$calid."' and id_user='".$users[$i]."'";
 	$res = $db->db_query($req);
 	if( $res && $db->db_num_rows($res) > 0)
 		{
 		$rr = $db->db_fetch_array($res);
-		$req = "update ".BAB_CALACCESS_USERS_TBL." set id_user='".$userid."', bwrite='".$baccess."' where id='".$rr['id']."'";
+		$req = "update ".BAB_CALACCESS_USERS_TBL." set id_user='".$users[$i]."', bwrite='".$baccess."' where id='".$rr['id']."'";
 		$res = $db->db_query($req);
 		}
 	else
 		{
-		$req = "insert into ".BAB_CALACCESS_USERS_TBL." (id_cal, id_user, bwrite) values ('".$calid."', '".$userid."', '".$baccess."')";
+		$req = "insert into ".BAB_CALACCESS_USERS_TBL." (id_cal, id_user, bwrite) values ('".$calid."', '".$users[$i]."', '".$baccess."')";
 		$res = $db->db_query($req);
 		}
+	}
 
 }
 
@@ -257,8 +421,10 @@ if(!isset($idx))
 
 if( isset($accessadd) )
 {
-	addAccessUsers($userid, $idcal, $baccess);
+	addAccessUsers($users, $idcal, $baccess);
+	$idx = "lunload";
 }
+
 if( isset($accessdel) )
 {
 	delAccessUsers($users, $idcal);
@@ -270,6 +436,19 @@ if( isset($modify) && $modify == "options")
 
 switch($idx)
 	{
+	case "lunload":
+		listUsersUnload();
+		exit;
+		break;
+	case "brow":
+		$idcal = bab_getCalendarId($BAB_SESS_USERID, 1);
+		if( $idcal != 0 )
+			{
+			browseUsers($pos, $cb, $idcal);
+			}
+		exit;
+		break;
+
 	case "access":
 		$babBody->title = bab_translate("Calendar Options");
 		$idcal = bab_getCalendarId($BAB_SESS_USERID, 1);
