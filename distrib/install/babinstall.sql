e6260f372f16b9e73064137b94dd0be65d5ffdda
nouaya <nouaya>
2002-10-10 07:16:06 +0000

    Minor change

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -163,9 +163,9 @@ CREATE TABLE bab_groups (
   PRIMARY KEY (id)
 );
 
-INSERT INTO bab_groups VALUES ( '1', 'Registered', 'All registered users', 'N', 'N', '0', 'N', 'N', 'Y', 'Y', 'Y');
-INSERT INTO bab_groups VALUES ( '2', 'Guests', 'all not registered users', 'N', 'N', '0', 'N', 'N', 'Y', 'N', 'N');
-INSERT INTO bab_groups VALUES ( '3', 'Administrators', 'Manage the site', 'N', 'N', '0', 'N', 'N', 'Y', 'Y', 'Y');
+INSERT INTO bab_groups VALUES ( '1', 'Registered', 'All registered users', 'N', 'N', '0', 'N', 'N', 'Y', 'Y', 'Y', 'N');
+INSERT INTO bab_groups VALUES ( '2', 'Guests', 'all not registered users', 'N', 'N', '0', 'N', 'N', 'Y', 'N', 'N', 'N');
+INSERT INTO bab_groups VALUES ( '3', 'Administrators', 'Manage the site', 'N', 'N', '0', 'N', 'N', 'Y', 'Y', 'Y', 'Y');
 
 
 # --------------------------------------------------------
