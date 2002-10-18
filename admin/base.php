dfdaa4a50fbdb7e4733cb80297141ba86384cc1b
nouaya <nouaya>
2002-10-18 08:33:50 +0000

    Minor change

--- admin/base.php
+++ admin/base.php
@@ -18,19 +18,5 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
  * USA.																	*
 ************************************************************************/
-if( !empty($_SERVER['PHP_SELF']))
-	$babPhpSelf = $_SERVER['PHP_SELF'];
-else
-	$babPhpSelf = $HTTP_SERVER_VARS['PHP_SELF'];
-
-if ($babPhpSelf != "/index.php")
-	{
-	if( !empty($_SERVER['HTTP_HOST']))
-		Header("Location: http://". $_SERVER['HTTP_HOST']."/index.php?tg=accden");
-	else
-		Header("Location: http://". $HTTP_SERVER_VARS['HTTP_HOST']."/index.php?tg=accden");
-	exit;
-	}
-
-$babInstallPath = "";
+exit;
 ?>
--- distrib/base.php
+++ distrib/base.php
@@ -1,20 +1,21 @@
 <?php
 /************************************************************************
  * Ovidentia                                                            *
- ************************************************************************
  * Copyright (c) 2001, CANTICO ( http://www.cantico.fr )                *
- ***********************************************************************/
-if( !empty($_SERVER['PHP_SELF']))
-	$babPhpSelf = $_SERVER['PHP_SELF'];
-else
-	$babPhpSelf = $HTTP_SERVER_VARS['PHP_SELF'];
-
-if ($babPhpSelf != "/index.php")
-	{
-	if( !empty($_SERVER['HTTP_HOST']))
-		Header("Location: http://". $_SERVER['HTTP_HOST']."/index.php?tg=accden");
-	else
-		Header("Location: http://". $HTTP_SERVER_VARS['HTTP_HOST']."/index.php?tg=accden");
-	exit;
-	}
+ ************************************************************************
+ * This program is free software; you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 2, or (at your option)  *
+ * any later version.													*
+ *																		*
+ * This program is distributed in the hope that it will be useful, but  *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of			*
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.					*
+ * See the  GNU General Public License for more details.				*
+ *																		*
+ * You should have received a copy of the GNU General Public License	*
+ * along with this program; if not, write to the Free Software			*
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
+ * USA.																	*
+************************************************************************/
 ?>
