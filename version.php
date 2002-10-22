bcb2970b983c1333a3f6b83b6e814e831f6edd49
nouaya <nouaya>
2002-10-22 15:15:40 +0000

    Minor change

--- version.php
+++ version.php
@@ -183,6 +183,8 @@ switch($idx)
 		break;
 
 	case "lang":
+		if( !bab_isUserAdministrator())
+			exit;
 		$ar = echoLang($GLOBALS['babInstallPath']);
 		$tab = array();
 		for( $i = 0; $i < count($ar); $i++)
@@ -233,6 +235,8 @@ switch($idx)
 
 	case "version":
 	default:
+		if( !bab_isUserAdministrator())
+			exit;
 		$str = getVersion();
 		break;
 	}
