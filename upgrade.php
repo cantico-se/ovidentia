8928f623b89a9db560a252d5fe159f8dc6e92962
nouaya <nouaya>
2002-11-13 14:51:40 +0000

    Minor change

--- upgrade.php
+++ upgrade.php
@@ -861,13 +861,17 @@ function upgrade341to342()
 $ret = "";
 $db = $GLOBALS['babDB'];
 
-$req = "ALTER TABLE ".BAB_TOPICS_TBL." ADD notify ENUM('N','Y') NOT NULL";
-$res = $db->db_query($req);
-if( !$res)
+$res = $db->db_query("SHOW COLUMNS from ".BAB_TOPICS_TBL." like 'notify'");
+if( !$res || $db->db_num_rows($res) == 0 )
 	{
-	$ret = "Alteration of <b>".BAB_TOPICS_TBL."</b> table failed !<br>";
-	return $ret;
-	}
+    $req = "ALTER TABLE ".BAB_TOPICS_TBL." ADD notify ENUM('N','Y') NOT NULL";
+    $res = $db->db_query($req);
+    if( !$res)
+        {
+        $ret = "Alteration of <b>".BAB_TOPICS_TBL."</b> table failed !<br>";
+        return $ret;
+        }
+    }
 
 $db->db_query("INSERT INTO ".BAB_INI_TBL." VALUES ('ver_prod', 'E')");
 
