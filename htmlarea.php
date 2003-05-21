d5d8ae8d7399cdf4fd9442cc9f73b2b3221cbd22
nouaya <nouaya>
2003-05-21 14:35:07 +0000

    First add:  htmlarea editor

---
+++ htmlarea.php
@@ -0,0 +1,116 @@
+<?
+/************************************************************************
+ * OVIDENTIA http://www.ovidentia.org                                   *
+ ************************************************************************
+ * Copyright (c) 2003 by CANTICO ( http://www.cantico.fr )              *
+ *                                                                      *
+ * This file is part of Ovidentia.                                      *
+ *                                                                      *
+ * Ovidentia is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 2, or (at your option)  *
+ * any later version.													*
+ *																		*
+ * This program is distributed in the hope that it will be useful, but  *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of			*
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.					*
+ * See the  GNU General Public License for more details.				*
+ *																		*
+ * You should have received a copy of the GNU General Public License	*
+ * along with this program; if not, write to the Free Software			*
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
+ * USA.																	*
+************************************************************************/
+include_once "base.php";
+
+function get_js_style_list()
+	{
+	if ($GLOBALS['babSkin'] == "ovidentia")
+
+		$filename = $GLOBALS['babInstallPath']."skins/".$GLOBALS['babSkin']."/styles/".$GLOBALS['babStyle'];
+	else
+		$filename = "skins/".$GLOBALS['babSkin']."/styles/".$GLOBALS['babStyle'];
+	$fcontents = file($filename);
+
+	$get=false;
+
+	while(list( $numero_ligne, $ligne ) = each( $fcontents))
+		{
+		if (trim($ligne) == "/*BAB_EDITOR_CSS_BEGIN*/") $get=true;
+		if (trim($ligne) == "/*BAB_EDITOR_CSS_END*/") $get=false;
+
+		// detecter si il y a un style, couper a l'accolade ou au deux points
+		$tmp = explode("{", $ligne);
+		$tmp = explode(":", $tmp[0]);
+		if (substr($tmp[0],0,1) == "." ) $tmp[0] = substr($tmp[0],1,(strlen($tmp[0])-1));
+		$tmp = explode(".", $tmp[0]);
+		if (isset($tmp[1])) $tmp[0]=$tmp[1];
+		$tmp = trim($tmp[0]);
+		if ($tmp != "" && substr($tmp,-2)!="*/" && substr($tmp,0,2)!="/*" && $tmp!="{" && $tmp!="}" && $get)
+			{
+			//echo $tmp."<br>";
+			$affichage = str_replace('_',' ',$tmp);
+			$jsligne[] = "\t\"".$affichage."\" : \"".$tmp."\"";
+			}
+		}
+
+	header("Content-type: application/x-javascript");
+
+
+	echo "HTMLArea.babstyle = {\n";
+
+	for ($i = 0 ; $i<(count($jsligne)-1) ; $i++ )
+		{
+		echo $jsligne[$i].",\n";
+		}
+	// ajouter la derniere ligne
+	echo $jsligne[count($jsligne)-1]."\n";
+
+	echo "};\n";
+	die();
+	}
+
+
+function get_css_style_list()
+	{
+	if ($GLOBALS['babSkin'] == "ovidentia")
+
+		$filename = $GLOBALS['babInstallPath']."skins/".$GLOBALS['babSkin']."/styles/".$GLOBALS['babStyle'];
+	else
+		$filename = "skins/".$GLOBALS['babSkin']."/styles/".$GLOBALS['babStyle'];
+	$fcontents = file($filename);
+
+	$get=false;
+	$counter = 0;
+	header("Content-type: text/css");
+
+	while(list( $numero_ligne, $ligne ) = each( $fcontents))
+		{
+		if (trim($ligne) == "/*BAB_EDITOR_CSS_BEGIN*/") $get=true;
+		if (trim($ligne) == "/*BAB_EDITOR_CSS_END*/") $get=false;
+
+		if ($get)
+			{
+			if ($counter!=0) echo $ligne;
+			$counter++;
+			}
+		}
+
+	die();
+	}
+
+
+
+/* main */
+
+switch($idx)
+	{
+	case "js":
+		get_js_style_list();
+		break;
+	case "css":
+		get_css_style_list();
+	default:
+		break;
+	}
+?>
---
+++ scripts/htmlarea/htmlarea-lang-nl-be.js
@@ -0,0 +1,45 @@
+// I18N constants
+
+HTMLArea.I18N = {
+	tooltips: {
+		bold:           "Gras",
+		italic:         "Italique",
+		underline:      "Souligné",
+		strikethrough:  "barré",
+		subscript:      "indice",
+		superscript:    "exposant",
+		justifyleft:    "Justifié à Gauche",
+		justifycenter:  "Justifié au centre",
+		justifyright:   "Justifié à Droite",
+		justifyfull:    "Tout Justifié",
+		orderedlist:    "Numéros",
+		unorderedlist:  "Puces",
+		outdent:        "Augementer la marge",
+		indent:         "Diminuer la marge",
+		forecolor:      "Couleur de la police",
+		backcolor:      "Couleur de fond",
+		horizontalrule: "Ligne horizontale",
+		createlink:     "Insérer un lien Web",
+		insertimage:    "Insérer une Image",
+		inserttable:    "Insérer un Tableau",
+		htmlmode:       "Voir la source HTML",
+		popupeditor:    "Editeur en plein écran",
+		about:          "A propos",
+		help:           "Aide",
+		textindicator:  "Style",
+		babimage:		"Insérer une image Ovidentia",
+		bablink:		"Insérer un lien web",
+		cleanhtml:		"Nettoyer le code HTML",
+		copy:			"Copier",
+		cut:			"Couper",
+		paste:			"Coller",
+		babfile:		"Insérer un fichier Ovidentia",
+		undo:			"Annuler",
+		redo:			"Rétablir"
+	}
+};
+
+
+HTMLArea.messages = {
+		cleanHTML:     "Attention : nettoyer le code HTML peut supprimer la mise en page du document"
+};
