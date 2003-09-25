ed499e3cda98ce46e5b8405df1c6f0e9515ec70b
pauder <pauder>
2003-09-25 13:26:39 +0000

    bug : #102 subdirectory 'editor' limitation for ovml article insertion

--- editorovml.php
+++ editorovml.php
@@ -76,11 +76,12 @@ function browse($url,$cb)
 				$upperpath = substr($url,0,strrpos($url,"/"));
 				$this->backlink = $GLOBALS['babUrlScript']."?tg=editorovml&url=".$upperpath;
 				}
+			$this->path = is_dir($GLOBALS['babOvmlPath'].'editor') ? $GLOBALS['babOvmlPath'].'editor/' : $GLOBALS['babOvmlPath'];
 			$this->url = $url;
 			$this->cb = "".$cb;
 			$this->ext = array (".ovml", ".html", ".htm", ".oml",".ovm");
-			$this->tablo_dir = dire_dir($GLOBALS['babOvmlPath'].$url);
-			$this->tablo_files = dire_ext($GLOBALS['babOvmlPath'].$url,$this->ext);
+			$this->tablo_dir = dire_dir($this->path.$url);
+			$this->tablo_files = dire_ext($this->path.$url,$this->ext);
 			$this->count_dir = count($this->tablo_dir);
 			if (is_array($this->tablo_files))
 				$this->count_files = count($this->tablo_files);
@@ -95,7 +96,7 @@ function browse($url,$cb)
 			static $i = 0;
 			if( $i < $this->count_dir)
 				{
-				$subfiles = dire_ext($GLOBALS['babOvmlPath'].$this->tablo_dir[$i],$this->ext);
+				$subfiles = dire_ext($this->path.$this->tablo_dir[$i],$this->ext);
 				if (count($subfiles)>0)
 					{
 					$this->displink = true;
