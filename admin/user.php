d17210bc223d343120b7f0d857f56758dafb4991
nouaya <nouaya>
2003-10-14 14:19:21 +0000

    Minor change

--- admin/user.php
+++ admin/user.php
@@ -358,7 +358,7 @@ function updateUser($id, $changepwd, $is_confirmed, $disabled, $group)
 	$res = $db->db_query("update ".BAB_USERS_TBL." set changepwd='$changepwd', is_confirmed='$is_confirmed', disabled='$disabled' where id='$id'");
 
 	$db = $GLOBALS['babDB'];
-	$db->db_query("update ".BAB_USERS_GROUPS_TBL." set isprimary='N'where id_object='$id'");
+	$db->db_query("update ".BAB_USERS_GROUPS_TBL." set isprimary='N' where id_object='$id'");
 	if( !empty($group))
 		{
 		$db->db_query("update ".BAB_USERS_GROUPS_TBL." set isprimary='Y'where id_object='$id' and id_group='$group'");
