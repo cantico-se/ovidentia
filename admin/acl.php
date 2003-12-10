b20c73442f272d577aacc3276c9e35a295ba6a13
pauder <pauder>
2003-12-10 08:43:44 +0000

    add : macl class, multiple tables management in one page

--- admin/acl.php
+++ admin/acl.php
@@ -22,164 +22,231 @@
  * USA.																	*
 ************************************************************************/
 include_once "base.php";
-function aclGroups($target, $index, $table, $id, $return)
+
+class macl
 	{
-	global $babBody;
-	if( !isset($id))
+	var $tables = array();
+	var $groupsfilter = array();
+	var $arr_listgroups = array();
+	var $arr_disabled = array();
+	var $arr_everybody = array();
+	var $arr_users = array();
+	var $arr_guests = array();
+	var $arr_groupsfilter = array();
+		
+	function macl($target, $index,$id, $return)
 		{
-		$babBody->msgerror = bab_translate("ERROR: You must choose a valid item !!");
-		return;
+		global $babBody;
+		$this->target = $target;
+		$this->index = $index;
+		$this->id = $id;
+		
+		$this->return = $return;
+		$this->name = bab_translate("Groups Names");
+		$this->updategroups = bab_translate("Update Groups");
+		$this->disabled = bab_translate("Disabled");
+		$this->everybody = bab_translate("Everybody");
+		$this->users = bab_translate("Registered Users");
+		$this->guests = bab_translate("Unregistered Users");
+		$this->listgroups = bab_translate("Groups List");
+		$this->db = $GLOBALS['babDB'];
+		$this->multiple = false;
+
+		if( $babBody->currentAdmGroup == 0 )
+			$this->ballsite = true;
+		else
+			$this->ballsite = false;
+		
+		
+
+		$req = "select * from ".BAB_GROUPS_TBL." where id > 2 and id_dgowner='".$babBody->currentAdmGroup."' order by name asc";
+		$this->res2 = $this->db->db_query($req);
+		$this->count2 = $this->db->db_num_rows($this->res2);
 		}
-	class temp
+		
+	function addtable($table,$name = '')
 		{
-		var $name;
-		var $updategroups;
-		var $disabled;
-		var $everybody;
-		var $users;
-		var $guests;
-		var $listgroups;
-		var $return;
-
-		var $arr = array();
-		var $what = array();
-		var $db;
-		var $id;
-		var $count;
-		var $res;
-		var $groups;
-		var $arrgroups;
-		var $select;
-		var $table;
-		var $target;
-		var $index;
-
-		var $ballsite;
-
-		function temp($target, $index, $table, $id, $return)
+		global $babBody;
+		$tblindex = count($this->tables);
+		if ($tblindex > 0) $this->multiple = true;
+		$this->tables[$tblindex]['table'] = $table;
+		$this->tables[$tblindex]['name'] = $name != '' ? $name : $table;
+		$this->tables[$tblindex]['what'] = array();
+		$this->tables[$tblindex]['what']['everybody'] = "";
+		$this->tables[$tblindex]['what']['users'] = "";
+		$this->tables[$tblindex]['what']['guests'] = "";
+		$this->tables[$tblindex]['what']['disabled'] = "";
+		$req = "select * from ".$table." where id_object='".$this->id."'";
+		$this->tables[$tblindex]['res'] = $this->db->db_query($req);
+		$this->tables[$tblindex]['count'] = $this->db->db_num_rows($this->tables[$tblindex]['res']);
+		if( $this->tables[$tblindex]['count'] < 1 )
+			$this->tables[$tblindex]['what']['disabled'] = "selected";			
+		else if( $this->tables[$tblindex]['count'] == 1)
 			{
-			global $babBody;
-			$this->table = $table;
-			$this->target = $target;
-			$this->index = $index;
-			$this->return = $return;
-			$this->name = bab_translate("Groups Names");
-			$this->updategroups = bab_translate("Update Groups");
-			$this->disabled = bab_translate("Disabled");
-			$this->everybody = bab_translate("Everybody");
-			$this->users = bab_translate("Registered Users");
-			$this->guests = bab_translate("Unregistered Users");
-			$this->listgroups = bab_translate("Groups List");
-			$this->db = $GLOBALS['babDB'];
-			$this->id = $id;
-			$this->what['everybody'] = "";
-			$this->what['users'] = "";
-			$this->what['guests'] = "";
-			$this->what['disabled'] = "";
-
-			if( $babBody->currentAdmGroup == 0 )
-				$this->ballsite = true;
-			else
-				$this->ballsite = false;
-			
-			$req = "select * from ".$table." where id_object='$id'";
-			$this->res1 = $this->db->db_query($req);
-			$this->count1 = $this->db->db_num_rows($this->res1);
-			if( $this->count1 < 1 )
-				$this->what['disabled'] = "selected";			
-			else if( $this->count1 == 1)
+			$arr = $this->db->db_fetch_array($this->tables[$tblindex]['res']);
+			if( $arr['id_group'] < 3)
 				{
-				$arr = $this->db->db_fetch_array($this->res1);
-				if( $arr['id_group'] < 3)
+				$this->count1 = 0;
+				switch($arr['id_group'])
 					{
-					$this->count1 = 0;
-					switch($arr['id_group'])
-						{
-						case 0:
-							$this->what['everybody'] = "selected";
-							break;
-						case 1:
-							$this->what['users'] = "selected";
-							break;
-						case 2:
-							$this->what['guests'] = "selected";
-							break;
-						}
-					}
-				else if( $babBody->currentAdmGroup != 0 && $arr['id_group'] == $babBody->currentAdmGroup )
-					{
-					$this->what['everybody'] = "selected";
+					case 0:
+						$this->tables[$tblindex]['what']['everybody'] = "selected";
+						break;
+					case 1:
+						$this->tables[$tblindex]['what']['users'] = "selected";
+						break;
+					case 2:
+						$this->tables[$tblindex]['what']['guests'] = "selected";
+						break;
 					}
 				}
-
-			$req = "select * from ".BAB_GROUPS_TBL." where id > 2 and id_dgowner='".$babBody->currentAdmGroup."' order by name asc";
-			$this->res2 = $this->db->db_query($req);
-			$this->count2 = $this->db->db_num_rows($this->res2);
-			}
-
-		function getnextgroup()
-			{
-			static $i = 0;
-			
-			if( $i < $this->count2)
+			else if( $babBody->currentAdmGroup != 0 && $arr['id_group'] == $babBody->currentAdmGroup )
 				{
-				$this->arrgroups = $this->db->db_fetch_array($this->res2);
-				if($this->count1 > 0)
-					{
-					$this->db->db_data_seek($this->res1, 0);
-					$this->arrgroups['select'] = "";
-					for( $j = 0; $j < $this->count1; $j++)
-						{
-						$this->groups = $this->db->db_fetch_array($this->res1);
-						if( $this->groups['id_group'] == $this->arrgroups['id'])
-							{
-							$this->arrgroups['select'] = "selected";
-							break;
-							}
-						}
-					}
+				$this->tables[$tblindex]['what']['everybody'] = "selected";
+				}
+			}
+		}
+		
+	function filter($listgroups = 0,$disabled = 0,$everybody = 0,$users = 0,$guest = 0,$groups = array())
+		{
+		$tblindex = count($this->tables) - 1;
+		$this->arr_listgroups[$tblindex] = $listgroups;
+		$this->arr_disabled[$tblindex] = $disabled;
+		$this->arr_everybody[$tblindex] = $everybody;
+		$this->arr_users[$tblindex] = $users;
+		$this->arr_guests[$tblindex] = $guest;
+		$this->arr_groupsfilter[$tblindex] = $groups;
+		}
+		
+	function getnexttable()
+		{
+		static $i = 0;
+		if( $i < count($this->tables))
+			{
+			$this->table = $this->tables[$i]['table'];
+			$this->title = $this->tables[$i]['name'];
+			$this->what = $this->tables[$i]['what'];
+			$this->res1 = $this->tables[$i]['res'];
+			$this->count1 = $this->tables[$i]['count'];
+			$this->listgroups = !empty($this->arr_listgroups[$i]) ? false : bab_translate("Groups List");
+			$this->disabled = !empty($this->arr_disabled[$i]) ? false : bab_translate("Disabled");
+			$this->everybody = !empty($this->arr_everybody[$i]) ? false : bab_translate("Everybody");
+			$this->users = !empty($this->arr_users[$i]) ? false : bab_translate("Registered Users");
+			$this->guests = !empty($this->arr_guests[$i]) ? false : bab_translate("Unregistered Users");
+			$this->groupsfilter = isset($this->arr_groupsfilter[$i]) && is_array($this->arr_groupsfilter[$i]) ? $this->arr_groupsfilter[$i] : array();
+			$i++;
+			return true;
+			}
+		else
+			{
+			$i = 0;
+			return false;
+			}
+		}
+
+	function getnextgroup(&$skip)
+		{
+		static $i = 0;
+		
+		if( $i < $this->count2)
+			{
+			$this->arrgroups = $this->db->db_fetch_array($this->res2);
+			if (in_array($this->arrgroups['id'],$this->groupsfilter))
+				{
+				$skip = true;
 				$i++;
 				return true;
 				}
-			else
+			$this->arrgroups['select'] = "";
+			if($this->count1 > 0)
 				{
-				return false;
+				$this->db->db_data_seek($this->res1, 0);
+				for( $j = 0; $j < $this->count1; $j++)
+					{
+					$this->groups = $this->db->db_fetch_array($this->res1);
+					if( $this->groups['id_group'] == $this->arrgroups['id'])
+						{
+						$this->arrgroups['select'] = "selected";
+						break;
+						}
+					}
 				}
-
+			$i++;
+			return true;
+			}
+		else
+			{
+			if ($this->count2 > 0)
+				$this->db->db_data_seek($this->res2, 0);
+			$i = 0;
+			return false;
 			}
 		}
-	$temp = new temp($target, $index, $table, $id, $return);
-	$babBody->babecho(	bab_printTemplate($temp, "acl.html", "aclgroups"));
+		
+	function babecho()
+		{
+		global $babBody;
+		$babBody->babecho(	bab_printTemplate($this, "acl.html", "maclgroups"));
+		}
 	}
 
-function aclUpdate($table, $id, $groups, $what)
+
+
+
+	
+function maclUpdate()
 	{
 	global $babBody;
-
 	$db = $GLOBALS['babDB'];
-	$req = "delete from ".$table." where id_object = '$id'";
-	$res = $db->db_query($req);
-
-	$arr = array();
-
-	if( $what == "")
+	//print_r($_POST);
+	$prefix = 'macl_what_';
+	foreach($_POST as $field => $value)
 		{
-		$cnt = count($groups);
-		if( $cnt > 0)
+		if (substr_count($field,$prefix) == 1)
 			{
-			for( $i = 0; $i < $cnt; $i++)
+			$table = substr($field,strlen($prefix));
+			$id = $_POST['item'];
+			$what = $_POST['macl_what_'.$table];
+			$groups = $_POST['macl_groups_'.$table];
+			
+			$req = "delete from ".$table." where id_object = '".$id."'";
+			$res = $db->db_query($req);
+		
+			$arr = array();
+		
+			if( $what == "")
 				{
-				$req = "insert into ".$table." (id_object, id_group) values ('". $id. "', '" . $groups[$i]. "')";
+				$cnt = count($groups);
+				if( $cnt > 0)
+					{
+					for( $i = 0; $i < $cnt; $i++)
+						{
+						$req = "insert into ".$table." (id_object, id_group) values ('". $id. "', '" . $groups[$i]. "')";
+						$res = $db->db_query($req);
+						}
+					}
+				}
+			else if( $what != -1)
+				{
+				if( $what == '0' && $babBody->currentAdmGroup != 0 )
+					$what = $babBody->currentAdmGroup;
+				$req = "insert into ".$table." (id_object, id_group) values ('". $id. "', '" . $what. "')";
 				$res = $db->db_query($req);
 				}
 			}
 		}
-	else if( $what != -1)
-		{
-		if( $what == '0' && $babBody->currentAdmGroup != 0 )
-			$what = $babBody->currentAdmGroup;
-		$req = "insert into ".$table." (id_object, id_group) values ('". $id. "', '" . $what. "')";
-		$res = $db->db_query($req);
-		}
+	}
+	
+function aclGroups($target, $index, $table, $id, $return)
+	{
+	global $babBody;
+	$macl = new macl($target, $index, $id, $return);
+	$macl->addtable($table);
+	$babBody->babecho(	bab_printTemplate($macl, "acl.html", "maclgroups"));
+	}
+
+function aclUpdate($table, $id, $groups, $what)
+	{
+	maclUpdate();
 	}
 ?>
