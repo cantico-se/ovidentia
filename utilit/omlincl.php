2e6726e36f24e7098c5903eca9b80e59368b4754
pauder <pauder>
2004-04-15 12:05:23 +0000

    change : OCRecentArticles articledate is modification date

--- utilit/omlincl.php
+++ utilit/omlincl.php
@@ -1830,7 +1830,7 @@ class bab_RecentArticles extends bab_handler
 				$this->ctx->curctx->push('ArticleReadMore', 1);
 			$this->ctx->curctx->push('ArticleId', $arr['id']);
 			$this->ctx->curctx->push('ArticleAuthor', $arr['id_author']);
-			$this->ctx->curctx->push('ArticleDate', bab_mktime($arr['date']));
+			$this->ctx->curctx->push('ArticleDate', bab_mktime($arr['date_modification']));
 			$this->ctx->curctx->push('ArticleUrl', $GLOBALS['babUrlScript']."?tg=articles&idx=More&topics=".$arr['id_topic']."&article=".$arr['id']);
 			$this->ctx->curctx->push('ArticlePopupUrl', $GLOBALS['babUrlScript']."?tg=articles&idx=viewa&topics=".$arr['id_topic']."&article=".$arr['id']);
 			$this->ctx->curctx->push('ArticleTopicId', $arr['id_topic']);
