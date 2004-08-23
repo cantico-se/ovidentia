15bcb3af468e394b512a942682b0b51426a08adc
nouaya <nouaya>
2004-08-23 15:08:25 +0000

    Minor change

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -958,7 +958,7 @@ CREATE TABLE bab_ini (
 
 INSERT INTO bab_ini VALUES ('ver_major', '5');
 INSERT INTO bab_ini VALUES ('ver_minor', '4');
-INSERT INTO bab_ini VALUES ('ver_build', '1');
+INSERT INTO bab_ini VALUES ('ver_build', '2');
 INSERT INTO bab_ini VALUES ('ver_prod', 'E');
 
 #
