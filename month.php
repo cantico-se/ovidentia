a06bf8d59e8b44478bade7a8eb8e394a9e4a9f75
pauder <pauder>
2004-09-24 08:48:47 +0000

    bug : calendar popup, day of the week from calendar options

--- month.php
+++ month.php
@@ -22,221 +22,17 @@
  * USA.																	*
 ************************************************************************/
 include_once "base.php";
-class babMonthX
-{
-var $currentMonth;
-var $currentYear;
-var $ymin;
-var $ymax;
-var $callback;
-var $curmonth;
-var $curyear;
-var $day3;
-
-var $days;
-var $daynumber;
-var $now;
-var $w;
-
-var $nextmonth;
-var $nextyear;
-var $prevmonth;
-var $prevyear;
-
-var $babCss;
-var $today;
-
-function babMonthX($month = "", $year = "", $callback = "")
-	{
-
-	$this->babCss = bab_printTemplate($this,"config.html", "babCss");
-	if(empty($month))
-		$this->currentMonth = Date("n");
-	else
-		{
-		$this->currentMonth = $month;
-		}
-	$this->callback = $callback;
-	
-	if(empty($year))
-		{
-		$this->currentYear = Date("Y");
-		}
-	else
-		{
-		$this->currentYear = $year;
-		}
-
-	$this->ymin = 1;
-	$this->ymax = 8;
-	$this->nextmonth = "";
-	$this->nextyear = "";
-	$this->prevmonth = "";
-	$this->prevyear = "";
-	$this->today = "";
-	}
-
-function setMaxYear( $delta )
-	{
-	$this->ymax = $delta;
-	}
-
-function setMinYear( $delta )
-	{
-	$this->ymin = $delta;
-	}
-
-function printout()
-	{
-	global $babMonths;
-	$this->curmonth = $babMonths[date("n", mktime(0,0,0,$this->currentMonth,1,$this->currentYear))];
-	$this->curyear = $this->currentYear;
-	$this->days = date("t", mktime(0,0,0,$this->currentMonth,1,$this->currentYear));
-	$this->daynumber = date("w", mktime(0,0,0,$this->currentMonth,1,$this->currentYear));
-	$this->now = date("j");
-	$this->w = 0;
-	$todaymonth = date("n");
-	$todayyear = date("Y"); 
-	if( $todayyear >= $this->currentYear - $this->ymin && $todayyear <= $this->currentYear + $this->ymax )
-		{
-		$this->today = "<a href=\"".$GLOBALS['babUrlScript']."?tg=month&callback=".$this->callback."&ymin=";
-		$this->today .= ($todayyear - $this->currentYear + $this->ymin)."&ymax=".($this->currentYear + $this->ymax - $todayyear)."&month=".$todaymonth."&year=".($todayyear)."\">".bab_translate("Go to Today")."</a>";
-		}
-
-	if( $this->currentYear > $this->currentYear - $this->ymin)
-		$this->prevyear = "<a href=\"".$GLOBALS['babUrlScript']."?tg=month&callback=".$this->callback."&ymin=".($this->ymin-1)."&ymax=".($this->ymax+1)."&month=".$this->currentMonth."&year=".($this->currentYear-1)."\"><<</a>";
-	else
-		$this->prevyear = "&nbsp;";
-
-	if( $this->currentMonth != 1 || $this->currentYear > $this->currentYear - $this->ymin)
-		{
-		if( $this->currentMonth == 1)
-			{
-			$this->prevmonth = "<a href=\"".$GLOBALS['babUrlScript']."?tg=month&callback=".$this->callback."&ymin=".($this->ymin-1)."&ymax=".($this->ymax+1)."&month=";
-			$this->prevmonth .= "12&year=".($this->currentYear-1);
-			}
-		else
-			{
-			$this->prevmonth = "<a href=\"".$GLOBALS['babUrlScript']."?tg=month&callback=".$this->callback."&ymin=".$this->ymin."&ymax=".$this->ymax."&month=";
-			$this->prevmonth .= ($this->currentMonth - 1)."&year=".$this->currentYear;
-			}
-		$this->prevmonth .= "\"><</a>";
-		}
-	else
-		$this->prevmonth = "&nbsp;";
-
-	if( $this->currentMonth != 12 || $this->currentYear < $this->currentYear + $this->ymax)
-		{
-		if( $this->currentMonth == 12)
-			{
-			$this->nextmonth = "<a href=\"".$GLOBALS['babUrlScript']."?tg=month&callback=".$this->callback."&ymin=".($this->ymin+1)."&ymax=".($this->ymax-1)."&month=";
-			$this->nextmonth .= "1&year=".($this->currentYear+1);
-			}
-		else
-			{
-			$this->nextmonth = "<a href=\"".$GLOBALS['babUrlScript']."?tg=month&callback=".$this->callback."&ymin=".$this->ymin."&ymax=".$this->ymax."&month=";
-			$this->nextmonth .= ($this->currentMonth+1)."&year=".$this->currentYear;
-			}
-		$this->nextmonth .= "\">></a>";
-		}
-	else
-		$this->nextmonth = "&nbsp;";
-
-
-	if( $this->currentYear < $this->currentYear + $this->ymax)
-		$this->nextyear = "<a href=\"".$GLOBALS['babUrlScript']."?tg=month&callback=".$this->callback."&ymin=".($this->ymin+1)."&ymax=".($this->ymax-1)."&month=".$this->currentMonth."&year=".($this->currentYear+1)."\">>></a>";
-	else
-		$this->nextyear = "&nbsp;";
-
-
-	echo bab_printTemplate($this,"month.html", "old");
-	}
-
-	function getnextday3()
-		{
-		global $babDays;
-		static $i = 0;
-		if( $i < 7)
-			{
-			$this->day3 = substr($babDays[$i], 0, 3);
-			$i++;
-			return true;
-			}
-		else
-			return false;
-		}
-
-	function getnextweek()
-		{
-		if( $this->w < 7)
-			{
-			$this->w++;
-			return true;
-			}
-		else
-			{
-			return false;
-			}
-		}
-
-	function getnextday()
-		{
-		static $d = 0;
-		static $total = 0;
-		if( $d < 7)
-			{
-			$this->currentday = false;
-
-			if( $this->w == 1 &&  $d < $this->daynumber)
-				{
-				$this->day = "&nbsp;";
-				$this->dayurl = "\"#\"";
-				}
-			else
-				{
-				$total++;
-
-				if( $total > $this->days)
-					return false;
-				if( $total == $this->now && date("n", mktime(0,0,0,$this->currentMonth,1,$this->currentYear)) == date("n") && $this->currentYear == date("Y"))
-					{
-					$this->currentday = true;
-					$this->dayurl = "\"#\" onclick=\"self.opener.".$this->callback."('".$total."','".$this->currentMonth."','".$this->currentYear."');window.close();\"";
-					$this->day = $total;
-					}
-				else
-					{
-					$this->dayurl = "\"#\" onclick=\"self.opener.".$this->callback."('".$total."','".$this->currentMonth."','".$this->currentYear."');window.close();\"";
-					$this->day = $total;
-					}
-
-				}
-			if( $total > $this->days)
-				{
-				return false;
-				}
-			$d++;
-			return true;
-			}
-		else
-			{
-			$d = 0;
-			return false;
-			}
-		}
-}
 
 
 class bab_calendar
 {
-
 
 function bab_calendar($month, $year, $callback, $ymin, $ymax)
 	{
 	$this->year = $year;
 	$this->month = $month;
 	$this->callback = $callback;
-	$this->ymin = $year - $ymin;
+	$this->ymin = $year - $ymin - 1;
 	$this->ymax = $year + $ymax;
 	$this->value = $this->ymin;
 	reset($GLOBALS['babMonths']);
@@ -248,6 +44,9 @@ function bab_calendar($month, $year, $callback, $ymin, $ymax)
 
 	$this->current_month = date('n');
 	$this->current_year = date('Y');
+
+
+	$this->startday = isset($GLOBALS['babBody']->icalendars->startday) ? $GLOBALS['babBody']->icalendars->startday : 1;
 	}
 
 
@@ -288,30 +87,23 @@ function getnextmonth()
 
 function getnextwday()
 	{
-	$this->text = substr(current($GLOBALS['babDays']),0,3);
-	if ($this->text)
+	static $i = 0;
+	
+	if ($i < 7)
 		{
-		next($GLOBALS['babDays']);
+		$index = $this->startday + $i < 7 ? $this->startday + $i : $this->startday + $i -7;
+		$this->text = substr($GLOBALS['babDays'][$index],0,3);
+		$i++;
 		return true;
 		}
 	else
 		{
-		reset($GLOBALS['babDays']);
 		return false;
 		}
 	}
 	
 }
 
-/*
-if( !isset($month)) { $month ='';}
-if( !isset($year)) { $year ='';}
-$m = new babMonthX($month, $year, $callback);
-$m->setMaxYear($ymax);
-$m->setMinYear($ymin);
-$m->printout();
-
-*/
 
 if (!isset($_GET['callback']))
 	die('missing callback');
@@ -322,6 +114,6 @@ $ymin = isset($_GET['ymin']) ? $_GET['ymin'] : 33; // minor than 1 Jan 1970 is r
 $ymax = isset($_GET['ymax']) ? $_GET['ymax'] : 60;
 
 $m = new bab_calendar($month, $year, $_GET['callback'], $ymin, $ymax);
-echo bab_printTemplate($m,"month.html", "new");
+echo bab_printTemplate($m,"month.html");
 
 ?>
