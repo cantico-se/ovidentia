a22444b63a652dd4fd38198b10413d90b47eb485
pauder <pauder>
2005-04-18 15:18:42 +0000

    bug : #543

--- utilit/dirincl.php
+++ utilit/dirincl.php
@@ -381,7 +381,7 @@ function summaryDbContact($id, $idu, $update=true)
 			$this->db = $GLOBALS['babDB'];
 			list($idgroup, $allowuu) = $this->db->db_fetch_array($this->db->db_query("select id_group, user_update from ".BAB_DB_DIRECTORIES_TBL." where id='".$id."'"));
 
-			$this->res = $this->db->db_query("select * from ".BAB_DBDIR_FIELDSEXTRA_TBL." where id_directory='".($idgroup != 0? 0: $id)."' order by list_ordering asc");
+			$this->res = $this->db->db_query("select * from ".BAB_DBDIR_FIELDSEXTRA_TBL." where id_directory='".($idgroup != 0? 0: $id)."' AND disabled='N' order by list_ordering asc");
 			if( $this->res && $this->db->db_num_rows($this->res) > 0)
 				{
 				$this->count = $this->db->db_num_rows($this->res);
