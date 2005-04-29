db8cd49e14890b42ed999738d8ccb828aabbefa0
pauder <pauder>
2005-04-29 15:54:37 +0000

    no message

--- utilit/addonapi.php
+++ utilit/addonapi.php
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
  * USA.																	*
 ************************************************************************/
-
+ 
 function bab_time($time)
 	{
 	if( $time < 0)
