4889b81bdcb8a8a24843f6c1768bde590f0a0c43
pauder <pauder>
2005-06-06 12:34:57 +0000

    bug : #606

--- utilit/gdiincl.php
+++ utilit/gdiincl.php
@@ -101,13 +101,53 @@ function bab_getResizedImage($imgf, $w, $h)
 					}
 				
 				function chkgd2(){
-					  $testGD = get_extension_funcs("gd"); // Grab function list
-					  if (!$testGD){ echo "GD not even installed."; exit; }
-					  if (in_array ("imagegd2",$testGD)) $gd_version = "<2"; // Check
-					  if ($gd_version == "<2") return false; else return true;
+					  if (function_exists('gd_info'))
+						{
+						$arr = gd_info();
+						preg_match('/\d/', $arr['GD Version'], $match);
+						if ( $match[0] >= 2) return true;
+						}
+
+					  return false;
 					}
 
-				if (chkgd2())
+
+				function gdVersion($user_ver = 0)
+					{
+					   static $gd_ver = 0;
+					   // Just accept the specified setting if it's 1.
+					   if ($user_ver == 1) { $gd_ver = 1; return 1; }
+					   // Use the static variable if function was called previously.
+					   if ($user_ver !=2 && $gd_ver > 0 ) { return $gd_ver; }
+					   // Use the gd_info() function if possible.
+					   if (function_exists('gd_info')) {
+						   $ver_info = gd_info();
+						   preg_match('/\d/', $ver_info['GD Version'], $match);
+						   $gd_ver = $match[0];
+						   return $match[0];
+					   }
+					   // If phpinfo() is disabled use a specified / fail-safe choice...
+					   if (preg_match('/phpinfo/', ini_get('disable_functions'))) {
+						   if ($user_ver == 2) {
+							   $gd_ver = 2;
+							   return 2;
+						   } else {
+							   $gd_ver = 1;
+							   return 1;
+						   }
+					   }
+					   // ...otherwise use phpinfo().
+					   ob_start();
+					   phpinfo(8);
+					   $info = ob_get_contents();
+					   ob_end_clean();
+					   $info = stristr($info, 'gd version');
+					   preg_match('/\d/', $info, $match);
+					   $gd_ver = $match[0];
+					   return $match[0];
+					} // End gdVersion()
+
+				if (gdVersion() >= 2)
 					{
 					$out = ImageCreateTrueColor($wimg, $himg);
 					imagecopyresampled($out, $tmp, 0, 0, 0, 0, $wimg, $himg, $imgsize[0], $imgsize[1]);
