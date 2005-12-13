0377724b7e9cf9078b8641922acd891a35c6a42a
pauder <pauder>
2005-12-13 11:52:11 +0000

    version 5.7.3

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -1009,7 +1009,7 @@ CREATE TABLE bab_ini (
 
 INSERT INTO bab_ini VALUES ('ver_major', '5');
 INSERT INTO bab_ini VALUES ('ver_minor', '7');
-INSERT INTO bab_ini VALUES ('ver_build', '2');
+INSERT INTO bab_ini VALUES ('ver_build', '3');
 INSERT INTO bab_ini VALUES ('ver_prod', 'E');
 
 #
