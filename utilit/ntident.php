751d451c6368f8e92456b77d4f70f6ef947555b8
laucho <laucho>
2006-09-21 14:06:39 +0000

    BUGFIX: Solved problem when using with register_globals = off. Now uses exclusively $_SESSION[] instead of session_register().

--- utilit/ntident.php
+++ utilit/ntident.php
@@ -24,50 +24,28 @@
 include_once "base.php";
 
 function NTuserLogin($nickname)
-	{
+{
 	global $babBody;
-	$sql="select * from ".BAB_USERS_TBL." where nickname='$nickname'";
+	$sql = "SELECT * FROM ".BAB_USERS_TBL." WHERE nickname='" . $nickname . "'";
 	$db = $GLOBALS['babDB'];
-	$result=$db->db_query($sql);
-	if ($db->db_num_rows($result) < 1)
-		{
+	$result = $db->db_query($sql);
+	if ($db->db_num_rows($result) < 1) {
 		$_SESSION['BAB_SESS_NTREGISTER'] = false;
 		$GLOBALS['BAB_SESS_NTREGISTER'] = false;
 		return false;
-		} 
-	else 
-		{
+	} else {
 		$arr = $db->db_fetch_array($result);
-		if( $arr['disabled'] == '1')
-			{
-			$_SESSION['BAB_SESS_NTREGISTER'] = false;
-			$GLOBALS['BAB_SESS_NTREGISTER'] = false;
-			return false;
-			}
-		if ($arr['is_confirmed'] == '1')
-			{
-			if( isset($_SESSION))
-				{
-				$_SESSION['BAB_SESS_NTREGISTER'] = true;
-				$_SESSION['BAB_SESS_NICKNAME'] = $arr['nickname'];
-				$_SESSION['BAB_SESS_FIRSTNAME'] = $arr['firstname'];
-				$_SESSION['BAB_SESS_LASTNAME'] = $arr['lastname'];
-				$_SESSION['BAB_SESS_USER'] = bab_composeUserName($arr['firstname'], $arr['lastname']);
-				$_SESSION['BAB_SESS_EMAIL'] = $arr['email'];
-				$_SESSION['BAB_SESS_USERID'] = $arr['id'];
-				$_SESSION['BAB_SESS_HASHID'] = $arr['confirm_hash'];
-				
-				$GLOBALS['BAB_SESS_NTREGISTER'] = true;
-				$GLOBALS['BAB_SESS_NICKNAME'] = $_SESSION['BAB_SESS_NICKNAME'];
-				$GLOBALS['BAB_SESS_FIRSTNAME'] = $_SESSION['BAB_SESS_FIRSTNAME'];
-				$GLOBALS['BAB_SESS_LASTNAME'] = $_SESSION['BAB_SESS_LASTNAME'];
-				$GLOBALS['BAB_SESS_USER'] = $_SESSION['BAB_SESS_USER'];
-				$GLOBALS['BAB_SESS_EMAIL'] = $_SESSION['BAB_SESS_EMAIL'];
-				$GLOBALS['BAB_SESS_USERID'] = $_SESSION['BAB_SESS_USERID'];
-				$GLOBALS['BAB_SESS_HASHID'] = $_SESSION['BAB_SESS_HASHID'];
-				}
-			else
-				{
+		if ($arr['disabled'] != '1' && $arr['is_confirmed'] == '1') {
+			if (isset($_SESSION)) {
+				$GLOBALS['BAB_SESS_NTREGISTER'] = $_SESSION['BAB_SESS_NTREGISTER'] = true;
+				$GLOBALS['BAB_SESS_NICKNAME'] = $_SESSION['BAB_SESS_NICKNAME'] = $arr['nickname'];
+				$GLOBALS['BAB_SESS_FIRSTNAME'] = $_SESSION['BAB_SESS_FIRSTNAME'] = $arr['firstname'];
+				$GLOBALS['BAB_SESS_LASTNAME'] = $_SESSION['BAB_SESS_LASTNAME'] = $arr['lastname'];
+				$GLOBALS['BAB_SESS_USER'] = $_SESSION['BAB_SESS_USER'] = bab_composeUserName($arr['firstname'], $arr['lastname']);
+				$GLOBALS['BAB_SESS_EMAIL'] = $_SESSION['BAB_SESS_EMAIL'] = $arr['email'];
+				$GLOBALS['BAB_SESS_USERID'] = $_SESSION['BAB_SESS_USERID'] = $arr['id'];
+				$GLOBALS['BAB_SESS_HASHID'] = $_SESSION['BAB_SESS_HASHID'] = $arr['confirm_hash'];
+			} else {
 				$GLOBALS['BAB_SESS_NICKNAME'] = $arr['nickname'];
 				$GLOBALS['BAB_SESS_FIRSTNAME'] = $arr['firstname'];
 				$GLOBALS['BAB_SESS_LASTNAME'] = $arr['lastname'];
@@ -75,77 +53,65 @@ function NTuserLogin($nickname)
 				$GLOBALS['BAB_SESS_EMAIL'] = $arr['email'];
 				$GLOBALS['BAB_SESS_USERID'] = $arr['id'];
 				$GLOBALS['BAB_SESS_HASHID'] = $arr['confirm_hash'];
-				}
-			return true;
 			}
-		else
-			{
+			return true;
+		} else {
 			$_SESSION['BAB_SESS_NTREGISTER'] = false;
 			$GLOBALS['BAB_SESS_NTREGISTER'] = false;
 			return false;
-			}
 		}
 	}
+}
 
-if ( substr($GLOBALS['babPhpSelf'],0,1) == '/' )
-		$babPhpSelf = substr($GLOBALS['babPhpSelf'],1);
-	else
-		$babPhpSelf = $GLOBALS['babPhpSelf'];
-	
-if (!session_is_registered('BAB_SESS_NTREGISTER'))
-	{
-	if (!isset($_COOKIE['ntident'])) setcookie("ntident","connexion");
-	session_register("BAB_SESS_NTREGISTER");
-	session_register("BAB_SESS_NTREGISTER2");
-	$GLOBALS['BAB_SESS_NTREGISTER2'] = false;
-	$GLOBALS['BAB_SESS_NTREGISTER'] = true;
+if (substr($GLOBALS['babPhpSelf'],0,1) == '/') {
+	$babPhpSelf = substr($GLOBALS['babPhpSelf'],1);
+} else {
+	$babPhpSelf = $GLOBALS['babPhpSelf'];
+}
+
+if (!isset($_SESSION['BAB_SESS_NTREGISTER'])) {
+	if (!isset($_COOKIE['ntident']))
+		setcookie('ntident', 'connexion');
+	$GLOBALS['BAB_SESS_NTREGISTER2'] = $_SESSION['BAB_SESS_NTREGISTER2'] = false;
+	$GLOBALS['BAB_SESS_NTREGISTER'] = $_SESSION['BAB_SESS_NTREGISTER'] = true;
 	header("location:".$GLOBALS['babUrl'].$babPhpSelf);
-	}
+}
 
-if (isset($NTidUser) && $GLOBALS['BAB_SESS_NTREGISTER'] && isset($_COOKIE['ntident']) && $_COOKIE['ntident'] == "connexion")
-	{
-	if (NTuserLogin($NTidUser))
-		{
+if (isset($NTidUser) && $GLOBALS['BAB_SESS_NTREGISTER'] && isset($_COOKIE['ntident']) && $_COOKIE['ntident'] == 'connexion') {
+	if (NTuserLogin($NTidUser)) {
 		$GLOBALS['BAB_SESS_NTREGISTER2'] = true;
 		$GLOBALS['BAB_SESS_NTREGISTER'] = false;
-		//setcookie("ntident","ok");
 		$db = $GLOBALS['babDB'];
-		$res=$db->db_query("select datelog from ".BAB_USERS_TBL." where id='".$BAB_SESS_USERID."'");
-		if( $res && $db->db_num_rows($res) > 0)
-			{
+		$res = $db->db_query("SELECT datelog FROM ".BAB_USERS_TBL." WHERE id='".$BAB_SESS_USERID."'");
+		if ($res && $db->db_num_rows($res) > 0) {
 			$arr = $db->db_fetch_array($res);
-			$db->db_query("update ".BAB_USERS_TBL." set datelog=now(), lastlog='".$arr['datelog']."' where id='".$BAB_SESS_USERID."'");
-			}
+			$db->db_query("UPDATE ".BAB_USERS_TBL." SET datelog=now(), lastlog='".$arr['datelog']."' WHERE id='".$BAB_SESS_USERID."'");
+		}
 
-		$res=$db->db_query("select * from ".BAB_USERS_LOG_TBL." where id_user='0' and sessid='".session_id()."'");
-		if( $res && $db->db_num_rows($res) > 0)
-			{
+		$res=$db->db_query("SELECT * FROM ".BAB_USERS_LOG_TBL." WHERE id_user='0' AND sessid='".session_id()."'");
+		if ($res && $db->db_num_rows($res) > 0) {
 			$arr = $db->db_fetch_array($res);
-			$db->db_query("update ".BAB_USERS_LOG_TBL." set id_user='".$BAB_SESS_USERID."' where id='".$arr['id']."'");
-			}
+			$db->db_query("UPDATE ".BAB_USERS_LOG_TBL." SET id_user='".$BAB_SESS_USERID."' WHERE id='".$arr['id']."'");
 		}
 	}
+}
 
 
-if( $GLOBALS['BAB_SESS_NTREGISTER'] && isset($_COOKIE['ntident']) && $_COOKIE['ntident'] == "connexion")
-    {
-    $babBody->script .= 
-        '//-->
-    </script>
-    <script type="text/javascript"> 
-    <!--
-    try {
-           var WshShell = new ActiveXObject("WScript.Network");
-           var query = \'\' + this.location;
-           if ( query.indexOf(\'?\') != -1 && query.indexOf (\'NTidUser\') == -1) {
-            window.location.href = query+"&NTidUser="+escape(WshShell.Username);
-        } else if (query.indexOf(\'NTidUser\') == -1) {
-            window.location.href = "'.$babPhpSelf.'?NTidUser="+escape( WshShell.Username);
-        }
+if ($GLOBALS['BAB_SESS_NTREGISTER'] && isset($_COOKIE['ntident']) && $_COOKIE['ntident'] == 'connexion') {
+    $babBody->script .= '
+	try {
+		var WshShell = new ActiveXObject("WScript.Network");
+		var query = \'\' + this.location;
+		if (query.indexOf(\'?\') != -1 && query.indexOf (\'NTidUser\') == -1) {
+			window.location.href = query+"&NTidUser="+escape(WshShell.Username);
+		} else if (query.indexOf(\'NTidUser\') == -1) {
+			window.location.href = "'.$babPhpSelf.'?NTidUser="+escape(WshShell.Username);
+		}
     } catch(e) { window.status = e.message; }
     ';
-    }
+}
 
-if( $GLOBALS['BAB_SESS_NTREGISTER2'] && $GLOBALS['BAB_SESS_USERID'] )
+if ($GLOBALS['BAB_SESS_NTREGISTER2'] && $GLOBALS['BAB_SESS_USERID']) {
 	$babCnxLink = 0;
+}
 ?>
