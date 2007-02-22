885f7aab275b28f95585eb01a495c3d68f66f3a6
pauder <pauder>
2007-02-22 15:33:04 +0000

    function bab_userTranslate

---
+++ utilit/translateincl.php
@@ -0,0 +1,47 @@
+<?php
+/************************************************************************
+ * OVIDENTIA http://www.ovidentia.org                                   *
+ ************************************************************************
+ * Copyright (c) 2003 by CANTICO ( http://www.cantico.fr )              *
+ *                                                                      *
+ * This file is part of Ovidentia.                                      *
+ *                                                                      *
+ * Ovidentia is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 2, or (at your option)  *
+ * any later version.													*
+ *																		*
+ * This program is distributed in the hope that it will be useful, but  *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of			*
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.					*
+ * See the  GNU General Public License for more details.				*
+ *																		*
+ * You should have received a copy of the GNU General Public License	*
+ * along with this program; if not, write to the Free Software			*
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
+ * USA.																	*
+************************************************************************/
+include_once "base.php";
+
+/**
+ * Additional functions for internationalisation
+ */
+ 
+ 
+ 
+
+/**
+ * Translate text with user language configuration
+ * @param	int		$id_user
+ * @param	string	$str
+ * @param	string	$folder
+ * @return	string
+ */
+function bab_userTranslate($id_user, $str, $folder='') {
+
+	$userlang = bab_getUserSetting($id_user, 'lang');
+	return bab_translate($str, $folder, $userlang);
+}
+
+
+?>
