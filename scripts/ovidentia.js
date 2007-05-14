6630199f218e56c8931b702408a95ceecdd0ace5
laucho <laucho>
2007-05-14 07:45:10 +0000

    BUGM #784: Error message in the planning when using IE.
    The bug could actually happen on any page using the function bab_showOnMouse from ovidentia.js.
    
    Merged from PATCHS-6-4-0

--- scripts/ovidentia.js
+++ scripts/ovidentia.js
@@ -69,130 +69,149 @@ function bab_selectFirstInputField(filterclass)
 		}
 	}
 
+
+
 var bab_popup_obj = null;
+
 function bab_popup(url,divisor,menubar)
-	{
-	if (!menubar)
-		{
+{
+	if (!menubar) {
 		menubar = 'no';
-		}
-	else
-		{
+	} else {
 		menubar = 'yes';
-		}
-	if (bab_popup_obj == null || bab_popup_obj.closed || bab_popup_obj == 0)
-		{
-		if (typeof divisor == 'undefined')
-			{
+	}
+	if (bab_popup_obj == null || bab_popup_obj.closed || bab_popup_obj == 0) {
+		if (typeof divisor == 'undefined') {
 				divisor = 3;
-			}
+		}
 		var wd = Math.round(screen.width/divisor);
 		var hd = Math.round(screen.height/divisor);
 		var w = screen.width-wd;
-		var h = menubar == 'yes' ? Math.round((screen.height-hd)/1.5) : screen.height-hd;
+		var h = menubar == 'yes' ? Math.round((screen.height - hd) / 1.5) : screen.height-hd;
 		var l = Math.round(wd/2);
 		var t = Math.round(hd/2);
 		var name = 'bab_popup'+Math.floor(Math.random() * 99999999);
 		
-		if (bab_popup_obj == 0)
-			{
+		if (bab_popup_obj == 0) {
 			window.open(url,name,'status=yes,menubar='+menubar+',personalbar=no,width='+w+',height='+h+',top='+t+',left='+l+',scrollbars=yes,resizable=yes');
 			bab_popup_obj = null;
-			}
-		else
-			{
+		} else {
 			bab_popup_obj = window.open(url,name,'status=yes,menubar='+menubar+',personalbar=no,width='+w+',height='+h+',top='+t+',left='+l+',scrollbars=yes,resizable=yes');
-			}
 		}
-	else
-		{
+	} else {
 		bab_popup_obj.focus();
 		bab_popup_obj.location.href = url;
+	}
+}
+
+
+/**
+ * @var Array	An array of functions that will be called when the page is loaded.
+ *
+ * Skins should not use the onload property of the body tag, but instead use this script
+ * somewhere after including ovidentia.js :
+ * bab_initFunctions.push(function() { ... });
+ */
+var bab_initFunctions = new Array();
+
+
+window.onload = function() {
+	window.isLoaded = true;
+	for (var i = 0; i < bab_initFunctions.length; i++) {
+		var initFunction = bab_initFunctions[i];
+		if (typeof(initFunction) == 'function') {
+			initFunction();
 		}
 	}
+};
 
 
-var bab_tooltip_obj = false;
+var bab_currentTooltip = null;
 
-function bab_showOnMouse(id,on)
-	{
-	var ie=document.all;
-	var ns6=document.getElementById && !document.all;
-	if (ie||ns6)
-		bab_tooltip_obj = document.all? document.all[id] : document.getElementById? document.getElementById(id) : "";
-	else
-		return false;
 
-	if (on)
-		{
-		try
-			{
-			document.getElementsByTagName('body')[0].appendChild(bab_tooltip_obj);
-			bab_tooltip_obj.style.position = 'absolute';
-			}
-		catch (e)
-			{
-			}
-		}
-	else
-		{
-		bab_tooltip_obj.style.visibility = 'hidden';
-		bab_tooltip_obj = false;
-		return true;
-		}
-
+function bab_showOnMouse(tooltipId, on)
+{
+	var tooltip = document.getElementById(tooltipId);
+	if (!tooltip) {
+		return;
 	}
+
+	if (window.isLoaded && !tooltip.bab_initialized) {
+		// Here we move the tooltip element at the end of the DOM tree to
+		// avoid an IE6 bug messing-up the z-index for positioned elements
+		// This way we are sure that the tooltip will appear above all other elements.
+		// On IE (again!) we must wait for the page to be completely loaded before we
+		// can touch the DOM.
+		document.getElementsByTagName('BODY')[0].appendChild(tooltip);
+		tooltip.bab_initialized = true;
+	}
+
+	if (on) {
+		window.bab_currentTooltip = tooltip;
+		document.onmousemove = bab_tooltipPosition;
+		tooltip.style.zIndex = 127;
+	} else {
+		document.onmousemove = null;
+		tooltip.style.visibility = 'hidden';
+		window.bab_currentTooltip = null;
+	}
+}
 
 
 function bab_tooltipPosition(e)
-	{
-	if (!bab_tooltip_obj)
+{
+	e = e || window.event;
+
+	var tooltip = window.bab_currentTooltip;
+	
+	if (!tooltip) {
 		return false;
-	var obj = bab_tooltip_obj;
-	var offsetxpoint=-60;
-	var offsetypoint=20;
-	var ie=document.all;
-	var ns6=document.getElementById && !document.all;
-
-	var ietypebody = (document.compatMode && document.compatMode!="BackCompat")? document.documentElement : document.body
-	var curX=(ns6)? e.pageX : event.clientX + ietypebody.scrollLeft;
-	var curY=(ns6)? e.pageY : event.clientY + ietypebody.scrollTop;
-	var rightedge=ie&&!window.opera ? ietypebody.clientWidth-event.clientX-offsetxpoint : window.innerWidth-e.clientX-offsetxpoint-20
-	var bottomedge=ie&&!window.opera ? ietypebody.clientHeight-event.clientY-offsetypoint : window.innerHeight-e.clientY-offsetypoint-20
-	var leftedge=(offsetxpoint<0)? offsetxpoint*(-1) : -1000;
-
-	if (rightedge<obj.offsetWidth)
-		{
-		obj.style.left=ie? ietypebody.scrollLeft + event.clientX-obj.offsetWidth+'px' : window.pageXOffset+e.clientX-obj.offsetWidth+'px';
-		}
-	else if (curX<leftedge)
-		{
-		obj.style.left='5px';
-		}
-	else
-		{
-		obj.style.left=curX+offsetxpoint+'px';
-		}
-
-	if (bottomedge < obj.offsetHeight)
-		obj.style.top=ie? ietypebody.scrollTop+event.clientY-obj.offsetHeight-offsetypoint+"px" : window.pageYOffset+e.clientY-obj.offsetHeight-offsetypoint+"px";
-	else
-		{
-		obj.style.top=curY+offsetypoint+"px"
-		}
-	obj.style.visibility="visible";
-	return true;
 	}
-	
-	
-function bab_getInstallPath() {
-	scripts = document.getElementsByTagName('script');
-	for(var i =0; i < scripts.length; i++) {
+
+	var offsetX = -tooltip.offsetWidth / 2, offsetY = 20;
+	for (var offsetParent = tooltip.offsetParent; offsetParent; offsetParent = offsetParent.offsetParent) {
+		offsetX -= offsetParent.offsetLeft || 0;
+		offsetY -= offsetParent.offsetTop || 0;
+	}
+
+	var ie = document.all;
+	var ns6 = document.getElementById && !document.all;
+
+	var ieBody = (document.compatMode && document.compatMode != 'BackCompat') ? document.documentElement : document.body;
+	var cursorX = ns6 ? e.pageX : e.clientX + ieBody.scrollLeft;
+	var cursorY = ns6 ? e.pageY : e.clientY + ieBody.scrollTop;
+	var rightedge = ie && !window.opera ? ieBody.clientWidth - e.clientX - offsetX : window.innerWidth - e.clientX - offsetX - 20;
+	var bottomedge = ie && !window.opera ? ieBody.clientHeight - e.clientY - offsetY : window.innerHeight - e.clientY - offsetY - 20;
+	var leftedge = (offsetX < 0) ? offsetX * (-1) : -1000;
+
+	if (rightedge < tooltip.offsetWidth) {
+		tooltip.style.left = ie ? ieBody.scrollLeft + e.clientX - tooltip.offsetWidth + 'px' : window.pageXOffset + e.clientX - tooltip.offsetWidth + 'px';
+	} else if (cursorX < leftedge) {
+		tooltip.style.left = '5px';
+	} else {
+		tooltip.style.left = cursorX + offsetX + 'px';
+	}
+
+	if (bottomedge < tooltip.offsetHeight) {
+		tooltip.style.top = ie ? ieBody.scrollTop + e.clientY - tooltip.offsetHeight - offsetY + 'px' : window.pageYOffset + e.clientY - tooltip.offsetHeight - offsetY + 'px';
+	} else {
+		tooltip.style.top = cursorY + offsetY + 'px';
+	}
+	tooltip.style.visibility = 'visible';
+	return true;
+}
+
+
+
+function bab_getInstallPath()
+{
+	var scripts = document.getElementsByTagName('script');
+	for (var i = 0; i < scripts.length; i++) {
 		if (-1 != scripts[i].src.indexOf('ovidentia.js')) {
 			arr = scripts[i].src.split('/');
 			for (var j in arr) {
 				if ('scripts' == arr[j]) {
-					return arr[j-1]+'/';
+					return arr[j-1] + '/';
 				}
 			}
 		}
