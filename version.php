8750e78d84d9d1c537dde57b84a231e1239d5476
pauder <pauder>
2007-07-13 16:11:23 +0000

    version management

--- version.php
+++ version.php
@@ -185,13 +185,16 @@ switch($idx)
 <META NAME="Description" CONTENT="">
 </HEAD>
 <BODY BGCOLOR="#FFFFFF">
-<center><h1>Ovidentia</h1>
-<?php
-echo $babSiteName . "<br>";
-echo bab_toHtml($str, BAB_HTML_ALL);
-?>
-<br>
-<p class="copyright">&copy; 2001, <a href="http://www.cantico.fr/">CANTICO</a> All rights reserved.</p></center>
+	<center>
+		<h1>Ovidentia</h1>
+		<?php
+		echo $babSiteName . "<br>";
+		echo bab_toHtml($str, BAB_HTML_ALL);
+		?>
+		<br>
+		<p><a href="?"><?php echo bab_translate("Home") ?></a></p>
+		<p class="copyright">&copy; 2001, <a href="http://www.cantico.fr/">CANTICO</a> All rights reserved.</p>
+	</center>
 </BODY>
 </HTML>
 
