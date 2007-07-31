804b415b1afbb4f41ac08b0dd7e4edd4cb8d2d16
laucho <laucho>
2007-07-31 16:55:08 +0000

    Version 6.4.94

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -1048,7 +1048,7 @@ CREATE TABLE bab_ini (
 
 INSERT INTO bab_ini VALUES ('ver_major', '6');
 INSERT INTO bab_ini VALUES ('ver_minor', '4');
-INSERT INTO bab_ini VALUES ('ver_build', '93');
+INSERT INTO bab_ini VALUES ('ver_build', '94');
 INSERT INTO bab_ini VALUES ('ver_prod', 'E');
 
 #
