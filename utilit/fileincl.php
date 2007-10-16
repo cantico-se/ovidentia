7a97a0090e6d177b67bc7336ef31dcebe741d7d8
nouaya <nouaya>
2007-10-16 16:13:13 +0000

    Incorrect full path

--- utilit/fileincl.php
+++ utilit/fileincl.php
@@ -89,7 +89,7 @@ function bab_getUploadFmPath($gr, $id)
 		$oFmFolder = $oFmFolderSet->get($oId->in($id));
 		if(!is_null($oFmFolder))
 		{
-			return $oFmFolder->getRelativePath() . '/';
+			return $oFmFolder->getRelativePath() . '/'.$oFmFolder->getName().'/';
 		}
 	}
 	else
