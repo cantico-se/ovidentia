f103679a6720df138c01a9b39b9849867d880915
laucho <laucho>
2007-11-19 12:07:02 +0000

    BUGFIX : db_fetch_array replaced by db_fetch_assoc. There seems to be a problem with db_fetch_array on PHP4.1.2 when a column has a NULL value.

--- utilit/ovmltm.php
+++ utilit/ovmltm.php
@@ -6,12 +6,12 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2, or (at your option)
 // any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
@@ -30,9 +30,9 @@ require_once $GLOBALS['babInstallPath'] . 'tmContext.php';
 
 /**
  * OVML Container <OCTmSpaces>
- * 
+ *
  * This container returns the list of Project Spaces viewable by the current user.
- * 
+ *
  * Returned OVML variables are :
  * - OVSpaceId
  * - OVSpaceName
@@ -51,10 +51,10 @@ class bab_TmSpaces extends bab_handler
 
 		$oTmCtx =& getTskMgrContext();
 		$conditions = array();
-//		if(($babBody->isSuperAdmin && $babBody->currentAdmGroup == 0) || $babBody->currentDGGroup['taskmanager'] == 'Y') {
-//		if ($babBody->currentAdmGroup) {
-//			$conditions[] = 'idDelegation = ' . $babDB->quote($babBody->currentAdmGroup);
-//		}
+		//		if(($babBody->isSuperAdmin && $babBody->currentAdmGroup == 0) || $babBody->currentDGGroup['taskmanager'] == 'Y') {
+		//		if ($babBody->currentAdmGroup) {
+			//			$conditions[] = 'idDelegation = ' . $babDB->quote($babBody->currentAdmGroup);
+			//		}
 		$conditions[] = 'id IN(' . $babDB->quote(array_keys($oTmCtx->getVisualisedIdProjectSpace())) . ')';
 		$query = "
 				SELECT
@@ -93,7 +93,7 @@ class bab_TmSpaces extends bab_handler
 
 		if ($this->idx < $this->count)
 		{
-			$space = $babDB->db_fetch_array($this->res);
+			$space = $babDB->db_fetch_assoc($this->res);
 			$this->ctx->curctx->push('CIndex', $this->idx);
 			$this->ctx->curctx->push('SpaceId', $space['id']);
 			$this->ctx->curctx->push('SpaceName', $space['name']);
@@ -144,30 +144,30 @@ class bab_TmProjects extends bab_handler
 		// We look for all the project for which the user has visualisation rights.
 		$this->projectIds = array();
 		$res = bab_selectProjectList($spaceId);
-		while ($project = $babDB->db_fetch_array($res))
+		while ($project = $babDB->db_fetch_assoc($res))
 		{
 			if (bab_isAccessValid(BAB_TSKMGR_PROJECTS_VISUALIZERS_GROUPS_TBL, $project['id']))
 			{
 				array_push($this->projectIds, $project['id']);
 			}
 		}
-		
+
 		$this->count = count($this->projectIds);
 		if ($this->count > 0)
 		{
-				$sql = 
+			$sql =
 				'SELECT ' .
 					'* ' .
 				' FROM ' .
-					BAB_TSKMGR_PROJECTS_TBL .
+			BAB_TSKMGR_PROJECTS_TBL .
 				' WHERE ' . 
 					'id IN (' . $babDB->quote($this->projectIds) . ')' .
 				' ORDER BY id';
-			
+
 			$this->res = $babDB->db_query($sql);
 			$this->count = $babDB->db_num_rows($this->res);
 		}
-		
+
 		$this->ctx->curctx->push('CCount', $this->count);
 	}
 
@@ -183,7 +183,7 @@ class bab_TmProjects extends bab_handler
 
 		if ($this->idx < $this->count)
 		{
-			$project = $babDB->db_fetch_array($this->res);
+			$project = $babDB->db_fetch_assoc($this->res);
 			if ( ! bab_isAccessValid(BAB_TSKMGR_PROJECTS_VISUALIZERS_GROUPS_TBL, $project['id']))
 			{
 				$skip = true;
@@ -222,7 +222,7 @@ class bab_TmProjects extends bab_handler
  * - OVTaskProjectId			The id of the task's project
  * - OVTaskNumber				The task number
  * - OVTaskShortDescription		The task short description
- * - OVTaskStartDate			
+ * - OVTaskStartDate
  * - OVTaskEndDate
  * - OVTaskCategoryId			The category id
  * - OVTaskCategoryName			The category name
@@ -259,55 +259,55 @@ class bab_TmTasks extends bab_handler
 						'TaskClass' => 'idClass'
 						);
 
-		$this->bab_handler($ctx);
-		$aFilter = array();
+						$this->bab_handler($ctx);
+						$aFilter = array();
 
-		if ($idProject = $ctx->get_value('projectid'))
-		{
-			// If the parameter 'projectid' is specified we will return the tasks belonging
-			// to this project if the user has visibility on the project.
-			$aFilter['iIdProject'] = $idProject;
-			if (!bab_isAccessValid(BAB_TSKMGR_PROJECTS_VISUALIZERS_GROUPS_TBL, $idProject))
-			{
-				$this->count = 0;
-				$this->ctx->curctx->push('CCount', $this->count);
-				return;
-			}
-		}
-		else
-		{
-			// If the parameter 'projectid' is NOT specified we return the user's personal tasks.
-			$aFilter['iIdOwner'] = $GLOBALS['BAB_SESS_USERID'];
-			$aFilter['isPersonnal'] = true;
-		}
-		if ($startDate = $ctx->get_value('startdate'))
-		{
-			$aFilter['sStartDate'] = $startDate;
-		}
-		if ($endDate = $ctx->get_value('enddate'))
-		{
-			$aFilter['sEndDate'] = $endDate;
-		}
+						if ($idProject = $ctx->get_value('projectid'))
+						{
+							// If the parameter 'projectid' is specified we will return the tasks belonging
+							// to this project if the user has visibility on the project.
+							$aFilter['iIdProject'] = $idProject;
+							if (!bab_isAccessValid(BAB_TSKMGR_PROJECTS_VISUALIZERS_GROUPS_TBL, $idProject))
+							{
+								$this->count = 0;
+								$this->ctx->curctx->push('CCount', $this->count);
+								return;
+							}
+						}
+						else
+						{
+							// If the parameter 'projectid' is NOT specified we return the user's personal tasks.
+							$aFilter['iIdOwner'] = $GLOBALS['BAB_SESS_USERID'];
+							$aFilter['isPersonnal'] = true;
+						}
+						if ($startDate = $ctx->get_value('startdate'))
+						{
+							$aFilter['sStartDate'] = $startDate;
+						}
+						if ($endDate = $ctx->get_value('enddate'))
+						{
+							$aFilter['sEndDate'] = $endDate;
+						}
 
-		// The default ordering is ascending on field 'TaskNumber'. 
-		$sortFields = array('sName' => 'sTaskNumber', 'sOrder' => 'ASC');
-		
-		// The 'orderby' parameter must contain the name of the column (OVML variable name without OV prefix) on which
-		// the container should be ordered.
-		if (($orderBy = $ctx->get_value('orderby')) && array_key_exists($orderBy, $columnNames))
-		{
-			$sortFields['sName'] = $columnNames[$orderBy];
-		}
-		// The 'order' parameter must contain 'asc' or 'desc'.
-		if (($order = $ctx->get_value('order')) && (strtoupper($order) == 'ASC' || strtoupper($order) == 'DESC'))
-		{
-			$sortFields['sOrder'] = strtoupper($order);
-		}
+						// The default ordering is ascending on field 'TaskNumber'.
+						$sortFields = array('sName' => 'sTaskNumber', 'sOrder' => 'ASC');
 
-		$sql = bab_selectTaskQuery($aFilter, $sortFields);
-		$this->res = $babDB->db_query($sql);
-		$this->count = $babDB->db_num_rows($this->res);
-		$this->ctx->curctx->push('CCount', $this->count);
+						// The 'orderby' parameter must contain the name of the column (OVML variable name without OV prefix) on which
+						// the container should be ordered.
+						if (($orderBy = $ctx->get_value('orderby')) && array_key_exists($orderBy, $columnNames))
+						{
+							$sortFields['sName'] = $columnNames[$orderBy];
+						}
+						// The 'order' parameter must contain 'asc' or 'desc'.
+						if (($order = $ctx->get_value('order')) && (strtoupper($order) == 'ASC' || strtoupper($order) == 'DESC'))
+						{
+							$sortFields['sOrder'] = strtoupper($order);
+						}
+
+						$sql = bab_selectTaskQuery($aFilter, $sortFields);
+						$this->res = $babDB->db_query($sql);
+						$this->count = $babDB->db_num_rows($this->res);
+						$this->ctx->curctx->push('CCount', $this->count);
 	}
 
 
@@ -322,7 +322,7 @@ class bab_TmTasks extends bab_handler
 
 		if ($this->idx < $this->count)
 		{
-			$task = $babDB->db_fetch_array($this->res);
+			$task = $babDB->db_fetch_assoc($this->res);
 			$this->ctx->curctx->push('CIndex', $this->idx);
 			$this->ctx->curctx->push('TaskId', $task['iIdTask']);
 			$this->ctx->curctx->push('TaskProjectId', $task['iIdProject']);
