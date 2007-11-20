7fb7fc38e1ce47ec4b0b12e36a0fb5b98b3ef82b
laucho <laucho>
2007-11-20 11:29:14 +0000

    BUGFIX : Wrong path for image file in imgget.php.

--- imgget.php
+++ imgget.php
@@ -1,45 +1,44 @@
 <?php
-/************************************************************************
- * OVIDENTIA http://www.ovidentia.org                                   *
- ************************************************************************
- * Copyright (c) 2003 by CANTICO ( http://www.cantico.fr )              *
- *                                                                      *
- * This file is part of Ovidentia.                                      *
- *                                                                      *
- * Ovidentia is free software; you can redistribute it and/or modify    *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2, or (at your option)  *
- * any later version.													*
- *																		*
- * This program is distributed in the hope that it will be useful, but  *
- * WITHOUT ANY WARRANTY; without even the implied warranty of			*
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.					*
- * See the  GNU General Public License for more details.				*
- *																		*
- * You should have received a copy of the GNU General Public License	*
- * along with this program; if not, write to the Free Software			*
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
- * USA.																	*
-************************************************************************/
+//-------------------------------------------------------------------------
+// OVIDENTIA http://www.ovidentia.org
+//
+// Ovidentia is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+//-------------------------------------------------------------------------
+/**
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @copyright Copyright (c) 2006 by CANTICO ({@link http://www.cantico.fr})
+ */
 include_once 'base.php';
 include_once $babInstallPath.'utilit/gdiincl.php';
 
 function getFmImage($idf, $w, $h)
-	{
+{
 	global $babDB;
-	include_once $GLOBALS['babInstallPath']."utilit/fileincl.php";
+	include_once $GLOBALS['babInstallPath'].'utilit/fileincl.php';
 
-	$res = $babDB->db_query("select * from ".BAB_FILES_TBL." where id='".$babDB->db_escape_string($idf)."'");
+	$res = $babDB->db_query("SELECT * FROM ".BAB_FILES_TBL." WHERE id=".$babDB->quote($idf));
 	if( $res && $babDB->db_num_rows($res) > 0 )
-		{
-		$arr = $babDB->db_fetch_array($res);
-		$fullpath = bab_getUploadFullPath($arr['bgroup'], $arr['id_owner']);
-		if( !empty($arr['path']))
-			$fullpath .= $arr['path']."/";
+	{
+		$file = $babDB->db_fetch_assoc($res);
+		$uploadpath = bab_FmFolderHelper::getUploadPath();
+		$fullpath = $uploadpath . $file['path'] . $file['name'];
 
-		return bab_getResizedImage($fullpath.$arr['name'], $w, $h);
-		}
+		return bab_getResizedImage($fullpath, $w, $h);
 	}
+}
 
 
 /* main */
