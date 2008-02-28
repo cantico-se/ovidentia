f7e61020ffc73a4796acf9183efa6b3daed037de
nouaya <nouaya>
2008-02-28 16:00:49 +0000

    Version 6.6.90

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -1052,7 +1052,7 @@ CREATE TABLE bab_ini (
 
 INSERT INTO bab_ini VALUES ('ver_major', '6');
 INSERT INTO bab_ini VALUES ('ver_minor', '6');
-INSERT INTO bab_ini VALUES ('ver_build', '0');
+INSERT INTO bab_ini VALUES ('ver_build', '90');
 INSERT INTO bab_ini VALUES ('ver_prod', 'E');
 
 #
