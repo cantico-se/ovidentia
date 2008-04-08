7958dfb53b9987048ae6c7660f9b26ef8c988d1b
samzeb <samzeb>
2008-04-08 14:40:25 +0000

    new version

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -1048,7 +1048,7 @@ CREATE TABLE bab_ini (
 
 INSERT INTO bab_ini VALUES ('ver_major', '6');
 INSERT INTO bab_ini VALUES ('ver_minor', '6');
-INSERT INTO bab_ini VALUES ('ver_build', '2');
+INSERT INTO bab_ini VALUES ('ver_build', '3');
 INSERT INTO bab_ini VALUES ('ver_prod', 'E');
 
 #
