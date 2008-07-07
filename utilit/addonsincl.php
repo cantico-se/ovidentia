2546d82424131cbc485095601f655ac880d43ffd
pauder <pauder>
2008-07-07 14:53:53 +0000

    fixed url encoding for addons in new addon api

--- utilit/addonsincl.php
+++ utilit/addonsincl.php
@@ -246,7 +246,7 @@ class bab_addonInfos {
 	 * @return string
 	 */
 	function getUrl() {
-		return $GLOBALS['babUrlScript'].'?tg=addon/'.$this->id_addon.'/';
+		return $GLOBALS['babUrlScript'].'?tg=addon%2F'.$this->id_addon.'%2F';
 	}
 
 	/**
