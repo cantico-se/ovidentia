7c71991f0b48511a726b6be63ddf0b52a5541398
laucho <laucho>
2009-02-03 17:12:12 +0000

    Fixed Collator::compare. We now assume that strings are always in UTF-8

--- utilit/i18n.class.php
+++ utilit/i18n.class.php
@@ -1,26 +1,26 @@
 <?php
-/************************************************************************
- * OVIDENTIA http://www.ovidentia.org                                   *
- ************************************************************************
- * Copyright (c) 2003 by CANTICO ( http://www.cantico.fr )              *
- *                                                                      *
- * This file is part of Ovidentia.                                      *
- *                                                                      *
- * Ovidentia is free software; you can redistribute it and/or modify    *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2, or (at your option)  *
- * any later version.													*
- *																		*
- * This program is distributed in the hope that it will be useful, but  *
- * WITHOUT ANY WARRANTY; without even the implied warranty of			*
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.					*
- * See the  GNU General Public License for more details.				*
- *																		*
- * You should have received a copy of the GNU General Public License	*
- * along with this program; if not, write to the Free Software			*
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
- * USA.																	*
-************************************************************************/
+//-------------------------------------------------------------------------
+// OVIDENTIA http://www.ovidentia.org
+//
+// Ovidentia is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+//-------------------------------------------------------------------------
+/**
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @copyright Copyright (c) 2008 by CANTICO ({@link http://www.cantico.fr})
+ */
 require_once 'base.php';
 
 
@@ -60,15 +60,9 @@ if(!class_exists('Collator'))
 		 */
 		public static function compare($sStr1, $sStr2)
 		{
-			if('utf8' == bab_charset::getDatabase())
-			{
-				$sStr1 = utf8_decode($sStr1);
-				$sStr2 = utf8_decode($sStr2);
-			}
+			$sStr1 = utf8_decode($sStr1);
+			$sStr2 = utf8_decode($sStr2);
 			return strnatcmp(bab_removeDiacritics($sStr1), bab_removeDiacritics($sStr2));
 		}
 	}
 } 
-
-
-?>
