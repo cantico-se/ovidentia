98693c1a3f127795bb19a5de6abd47a0e58fa157
laucho <laucho>
2009-02-09 15:57:24 +0000

    Fixed php5 parent::__construct call.

--- admin/topcats.php
+++ admin/topcats.php
@@ -1,37 +1,48 @@
 <?php
-/************************************************************************
- * OVIDENTIA http://www.ovidentia.org                                   *
- ************************************************************************
- * Copyright (c) 2003 by CANTICO ( http://www.cantico.fr )              *
- *                                                                      *
- * This file is part of Ovidentia.                                      *
- *                                                                      *
- * Ovidentia is free software; you can redistribute it and/or modify    *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2, or (at your option)  *
- * any later version.													*
- *																		*
- * This program is distributed in the hope that it will be useful, but  *
- * WITHOUT ANY WARRANTY; without even the implied warranty of			*
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.					*
- * See the  GNU General Public License for more details.				*
- *																		*
- * You should have received a copy of the GNU General Public License	*
- * along with this program; if not, write to the Free Software			*
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
- * USA.																	*
-************************************************************************/
+//-------------------------------------------------------------------------
+// OVIDENTIA http://www.ovidentia.org
+//
+// Ovidentia is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+//-------------------------------------------------------------------------
+/**
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @copyright Copyright (c) 2008 by CANTICO ({@link http://www.cantico.fr})
+ */
 require_once 'base.php';
 require_once dirname(__FILE__) . '/acl.php';
 require_once dirname(__FILE__) . '/../utilit/topincl.php';
 require_once dirname(__FILE__) . '/../utilit/tree.php';
 
 
+
+/**
+ * This treeview is used to perform administrative tasks on categories and article topics.
+ * 
+ *
+ */
 class bab_AdmArticleTreeView extends bab_ArticleTreeView
 {
+	/**
+	 * Constructor.
+	 *
+	 * @param $sId		A unique id 
+	 */
 	function bab_AdmArticleTreeView($sId)
 	{
-		parent::bab_ArticleTreeView($sId);
+		parent::__construct($sId);
 	}
 
 	function onElementAppended(&$oElement, $sIdParent)
