10dc8d06a4957aeb0760aede6f01f74980138ef6
laucho <laucho>
2009-02-18 15:36:45 +0000

    BUGM #1823 : Database Error: Field 'evt_info' doesn't have a default value.

--- upgrade.php
+++ upgrade.php
@@ -5701,5 +5701,7 @@ function ovidentia_upgrade($version_base,$version_ini) {
 	 */
 	$babDB->db_query("ALTER TABLE ".BAB_GROUPS_TBL." CHANGE `nb_set` `nb_set` INT( 10 ) UNSIGNED NOT NULL DEFAULT '0'"); 
 	
+	$babDB->db_query("ALTER TABLE ".BAB_STATS_EVENTS_TBL."CHANGE `evt_info` `evt_info` TEXT NOT NULL DEFAULT ''");
+
 	return true;
 }
