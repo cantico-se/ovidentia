9a14f6406fca3e372cbf896c9bd036015c0edb04
laucho <laucho>
2009-07-02 16:32:50 +0000

    FIxed usage of parameter force=1 for forcing upgrading ovidentia. The parameter force=1 will now work only if used by an administrator.

--- utilit/upgradeincl.php
+++ utilit/upgradeincl.php
@@ -327,7 +327,11 @@ function bab_upgrade($core_dir, &$ret, $forceUpgrade = false)
 
 	list($bab_ver_major, $bab_ver_minor, $bab_ver_build) = explode('.',$ini->getVersion());
 
-	if (!($forceUpgrade && bab_isUserAdministrator()) && ($ver_from == $ini->getVersion())) {
+
+	//TODO Should be replaced with a proper function.
+	$isUserAdministrator = $_SESSION['bab_groupAccess']['usergroups'][BAB_ADMINISTRATOR_GROUP];
+
+	if (!($forceUpgrade && $isUserAdministrator) && ($ver_from == $ini->getVersion())) {
 		$ret = bab_translate('You site is already up to date');
 		return false;
 	}
