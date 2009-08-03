e9be51f6ae03b2c5d7540d0bfdf41e1c94453959
laucho <laucho>
2009-08-03 13:19:53 +0000

    Fixed creation of directory and parents to avoid some strange bug of is_dir() sometimes reporting that some existing directories are not directories.

--- utilit/upgradeincl.php
+++ utilit/upgradeincl.php
@@ -1,27 +1,26 @@
 <?php
-/************************************************************************
- * OVIDENTIA http://www.ovidentia.org                                   *
- ************************************************************************
- * Copyright (c) 2003 by CANTICO ( http://www.cantico.fr )              *
- *                                                                      *
- * This file is part of Ovidentia.                                      *
- *                                                                      *
- * Ovidentia is free software; you can redistribute it and/or modify    *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation; either version 2, or (at your option)  *
- * any later version.													*
- *																		*
- * This program is distributed in the hope that it will be useful, but  *
- * WITHOUT ANY WARRANTY; without even the implied warranty of			*
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.					*
- * See the  GNU General Public License for more details.				*
- *																		*
- * You should have received a copy of the GNU General Public License	*
- * along with this program; if not, write to the Free Software			*
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
- * USA.																	*
-************************************************************************/
-include_once "base.php";
+//-------------------------------------------------------------------------
+// OVIDENTIA http://www.ovidentia.org
+// Ovidentia is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+//-------------------------------------------------------------------------
+/**
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @copyright Copyright (c) 2009 by CANTICO ({@link http://www.cantico.fr})
+ */
+include_once 'base.php';
 
 
 class BabDirectoryFilter
@@ -137,22 +136,25 @@ function bab_recursive_cp($wf, $wto) {
 
 
 function bab_cpaddons($from, $to, &$message)
-	{
+{
 
-	function create($path)
+	// Creates the specified directory. No error if $path already exists, make parent directories as needed.
+	function createDirectoryAndParents($path)
 	{
-	$el = explode("/",$path);
-	$memo = '';
-	foreach ($el as $rep)
-		{
-		if (!is_dir($memo.$rep) && !empty($rep)) { 
-			if (!bab_mkdir($memo.$rep)) {
-				return 	sprintf(bab_translate("Error : can't create directory : %s"), $memo.$rep);
+		if (is_dir($path)) {
+			return true;
+		}
+		$parentPath = dirname($path);
+		if (!is_dir($parentPath)) {
+			$parentPathCreated = createDirectoryAndParents($parentPath);
+			if ($parentPathCreated !== true) {
+				return $parentPathCreated;
 			}
 		}
-		$memo = $memo.$rep."/";
+		if (!bab_mkdir($path)) {
+			return sprintf(bab_translate("Error : can't create directory : %s"), $path);
 		}
-	return true;
+		return true;
 	}
 
 	if (mb_substr($from,-1) != "/") $from.="/";
@@ -167,7 +169,7 @@ function bab_cpaddons($from, $to, &$message)
 			);
 
 	foreach ($loc as $path) {
-		$creation = create($to.$path);
+		$creation = createDirectoryAndParents($to.$path);
 
 		if (true !== $creation) {
 			$message = $creation;
@@ -184,8 +186,8 @@ function bab_cpaddons($from, $to, &$message)
 
 	return true;
 }
-	
-	
+
+
 function bab_writeConfig($replace)
 	{
 	global $babBody;
