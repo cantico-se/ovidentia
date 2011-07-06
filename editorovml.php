0621d77f3ef48140b5d8ff8f9c5b0c0a282102bf
laucho <laucho>
2011-07-06 09:10:10 +0000

    Fixed vulnerability : checks added to url parameter in editorovml.

--- editorovml.php
+++ editorovml.php
@@ -23,31 +23,31 @@
 ************************************************************************/
 include_once 'base.php';
 
-function dire_ext($rep,$ext )
+function dire_ext($rep, $ext)
 {
 	$fichier = array();
-	if(!is_dir($rep))
-	{
-		return false;
+
+	if (!is_dir($rep)) {
+		return $fichier;
 	}
-	
+
 	$reper = opendir($rep);
 	$i = 0;
-	
-	while($dir = readdir($reper))
-	{
-		if(($dir != '.') && ($dir != '..')) 
-		{
+
+	while ($dir = readdir($reper)) {
+		if (($dir != '.') && ($dir != '..')) {
 			$iPos = mb_strpos($dir, '.');
-			if(false !== $iPos && in_array(mb_substr($dir, $iPos), $ext))
-			{
-				$fichier[$i] = $dir ; 
+			if (false !== $iPos && in_array(mb_substr($dir, $iPos), $ext)) {
+				$fichier[$i] = $dir ;
 				$i++;
 			}
 		}
 	}
+
 	return $fichier;
 }
+
+
 
 function dire_dir($rep )
 {
@@ -55,118 +55,126 @@ function dire_dir($rep )
 	$reper = opendir($rep);
 	$i = 0;
 	$fichier = array();
-	while($dir = readdir($reper))
-		{
-		if (($dir != '.') && ($dir != '..') && is_dir($rep.'/'.$dir) ) 
-			{
-			$fichier[$i] = $dir ; 
+	while($dir = readdir($reper)) {
+		if (($dir != '.') && ($dir != '..') && is_dir($rep.'/'.$dir) ) {
+			$fichier[$i] = $dir ;
 			$i++;
-			}
-		
 		}
+	}
 
 	return $fichier;
 }
 
 
+
 function browse($url)
-	{
+{
 	global $babBody, $babDB;
 
 	class temp
-		{
+	{
 		var $cb;
 		var $db;
 		var $count;
 
 		function temp($url)
-			{
-			if ( $url != "" ) 
-				{
+		{
+			// We sanitize $url.
+			$url = new bab_Path($url);
+			$url = $url->tostring();
+
+			if ($url != '') {
 				$this->backlink = true;
-				$upperpath = mb_substr($url,0,mb_strrpos($url,"/"));
-				$this->backlink = bab_toHtml($GLOBALS['babUrlScript']."?tg=editorovml&url=".$upperpath);
-				}
+				$upperpath = mb_substr($url, 0, mb_strrpos($url, '/'));
+				$this->backlink = bab_toHtml($GLOBALS['babUrlScript'] . '?tg=editorovml&url=' . $upperpath);
+			}
 			$this->path = is_dir($GLOBALS['babOvmlPath'].'editor') ? 'editor/' : '';
 			$this->url = $url;
-			$this->ext = array (".ovml", ".html", ".htm", ".oml",".ovm");
-			$this->tablo_dir = dire_dir($GLOBALS['babOvmlPath'].$this->path.$this->url);
-			
-			$this->tablo_files = dire_ext($GLOBALS['babOvmlPath'].$this->path.$this->url,$this->ext);
-			$this->count_dir = count($this->tablo_dir);
-		
-			if (is_array($this->tablo_files))
-				$this->count_files = count($this->tablo_files);
-			else
-				$this->count_files = 0;
-			
+			$this->ext = array('.ovml', '.html', '.htm', '.oml', '.ovm');
+
+			$urlPath = new bab_Path($GLOBALS['babOvmlPath'] . $this->path . $this->url);
+			$urlPath = $urlPath->getRealPath();
+
+			$ovmlPath = new bab_Path($GLOBALS['babOvmlPath']);
+			$ovmlPath = $ovmlPath->getRealPath();
+
+			if (mb_substr($urlPath, 0, mb_strlen($ovmlPath)) !== $ovmlPath) {
+				$this->tablo_dir = array();
+				$this->tablo_files = array();
+			} else {
+				$this->tablo_dir = dire_dir($GLOBALS['babOvmlPath'].$this->path.$this->url);
+				$this->tablo_files = dire_ext($GLOBALS['babOvmlPath'].$this->path.$this->url,$this->ext);
 			}
 
+			$this->count_dir = count($this->tablo_dir);
+			$this->count_files = count($this->tablo_files);
+		}
+
+
 		function getnext_dir()
-			{
+		{
 			global $babBody;
 			static $i = 0;
-			if( $i < $this->count_dir)
-				{
-				$this->upurl = bab_toHtml($GLOBALS['babUrlScript']."?tg=editorovml&url=".$this->tablo_dir[$i]);
+			if ($i < $this->count_dir) {
+				$this->upurl = bab_toHtml($GLOBALS['babUrlScript'] . '?tg=editorovml&url=' . $this->tablo_dir[$i]);
 				$this->title = bab_toHtml($this->tablo_dir[$i]);
 				$i++;
 				return true;
-				}
-			else
-				return false;
 			}
-		
+			return false;
+		}
+
+
 		function getnext_file()
-			{
+		{
 			global $babBody;
 			static $i = 0;
-			if( $i < $this->count_files)
-				{
+			if ($i < $this->count_files) {
 				$this->file = $this->tablo_files[$i];
-				if ($this->url != "")
-					{
-					$url=$this->url."/";
-					}
-				else
-					$url='';
+				if ($this->url != '') {
+					$url=$this->url.'/';
+				} else {
+					$url = '';
+				}
 				$this->urlfile = $this->path.$url.$this->tablo_files[$i];
-					
+
 				$i++;
 				return true;
-				}
-			else
-				return false;
 			}
+			return false;
 		}
-		
+	}
+
 	global $babBody;
-		
+
 	$babBody->setTitle(bab_translate('Ovml'));
 	$babBody->addStyleSheet('text_toolbar.css');
-	
+
 	$temp = new temp($url);
-	$babBody->babPopup(bab_printTemplate($temp,"editorovml.html", "editorovml"));
+	$babBody->babPopup(bab_printTemplate($temp, 'editorovml.html', 'editorovml'));
+}
+
+
+
+if (!isset($url)) {
+	$url = '';
+} else {
+	if (false !== mb_strpos($url, '..')) {
+		$url = '';
 	}
+}
 
-if(!isset($url))
-	{
-	$url = "";
-	}
+if (!isset($cb)) {
+	$cb = 'EditorOnInsertOvml';
+}
 
-if(!isset($cb))
-	{
-	$cb = "EditorOnInsertOvml";
-	}
+if (!isset($idx)) {
+	$idx = 'browse';
+}
 
-if (!isset($idx))
-	$idx = "browse";
-
-switch($idx)
-	{
+switch ($idx) {
 	default:
-	case "browse":
+	case 'browse':
 		browse($url);
 		exit;
-	}
-?>
+}
