5643791d93e49f3e4f457e47d294799abe1ab05a
pauder <pauder>
2011-11-09 10:19:44 +0000

    IE 8 Bug

--- artedit.php
+++ artedit.php
@@ -959,6 +959,10 @@ function bab_ajaxTopicRow() {
  */
 function bab_ajaxAttachments()
 {
+	// Bug IE, need cache control
+	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+	
 	$W = bab_Widgets();
 	$W->HtmlCanvas();
 	
