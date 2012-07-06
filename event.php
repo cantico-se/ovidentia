6f26ae43f8fe5ab3c497912ad2874155a097f283
laucho <laucho>
2012-07-06 15:38:30 +0000

    Removed extra {

--- event.php
+++ event.php
@@ -1241,7 +1241,7 @@ function confirmDeleteEvent($calid, $bupdrec, $notify)
 	}
 
 	// test access on all collection
-	foreach ($collectionPeriods as $period) {
+	foreach ($collectionPeriods as $period)
 	{
 
 		if (!$calendar->canDeleteEvent($period))
