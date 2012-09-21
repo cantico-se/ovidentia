864d8b92860c9bd67e8aeeb7c4a8437566e004ee
pauder <pauder>
2012-09-21 07:49:48 +0000

    add security check

--- utilit/session.class.php
+++ utilit/session.class.php
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
  * USA.																	*
 ************************************************************************/
-
+include_once "base.php";
 
 
 class bab_Session
