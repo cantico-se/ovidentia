e19eed25b2bbbbf09b6ac0a1a4ebf00ee8c700d4
laucho <laucho>
2012-10-23 12:26:26 +0000

    Fixed bab_CalendarPeriod::getOrganizer() usinf wrong variable.

--- utilit/cal.calendarperiod.class.php
+++ utilit/cal.calendarperiod.class.php
@@ -35,67 +35,67 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	 * Timestamp begin date
 	 * @var int
 	 */
-	public 	$ts_begin;	
+	public 	$ts_begin;
 
 	/**
 	 * Timestamp end date
 	 * @var int
 	 */
 	public 	$ts_end;
-	
+
 	/**
 	 * Can be set manually if an event is "free" to not interfere in availability search
 	 * this parameter is only used by the program, to apply the user preference for a free or busy event period, the period transparency "TRANSP" iCalendar parameter is used
 	 * the available public property will not be saved with the event
-	 * 
+	 *
 	 * @var bool
 	 */
 	public	$available;
-	
+
 	/**
 	 * Non-iCal data
 	 * @var mixed
 	 */
 	private $data = array();
 
-	
+
 	/**
 	 * collection associated to period
 	 * @var bab_PeriodCollection
 	 */
 	private $periodCollection;
-	
-	
+
+
 	/**
 	 * VALAM associated to VEVENT
 	 * @var bab_CalendarAlarm
 	 */
 	private $alarm;
 
-	
+
 	/**
 	 * If the same event is displayed more than once in the same page, contain the calendar associated for visualisation
 	 * @var string
 	 */
 	private $uiCalendar = null;
-	
-	
+
+
 	/**
 	 * If the same event is displayed more than once in the same page
 	 * @var string
 	 */
 	private $uiIdentifier = null;
-	
-	
-	
-	
+
+
+
+
 
 	/**
 	 * The timestamp in constructor parameters will not initialize the DTSTART and DTEND iCalendar properties
 	 * but for periods with no need of iCalendar propoerties, this is more efficient (availability calculation, working hours...)
-	 * 
+	 *
 	 * @see bab_CalendarPeriod::setDates()
-	 * 
+	 *
 	 * @param 	int						$begin		timestamp
 	 * @param 	int						$end		timestamp
 	 *
@@ -105,57 +105,57 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		$this->ts_begin 	= $begin;
 		$this->ts_end		= $end;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getName()
 	{
 		return 'VEVENT';
 	}
-	
+
 	/**
 	 * Initialize dates of period, this method will initialize the DTSTART and DTEND properties
-	 * 
+	 *
 	 * @param BAB_DateTime $begin
 	 * @param BAB_DateTime $end
 	 * @return bab_CalendarPeriod
 	 */
 	public function setDates(BAB_DateTime $begin, BAB_DateTime $end) {
-		
+
 		$this->setBeginDate($begin);
-		$this->setEndDate($end); 
-		
+		$this->setEndDate($end);
+
 		return $this;
 	}
-	
+
 	/**
 	 * @param BAB_DateTime $begin
 	 * @param bab_CalendarPeriod
 	 */
 	public function setBeginDate(BAB_DateTime $begin) {
-		
+
 		$this->ts_begin = $begin->getTimeStamp();
 		$this->setProperty('DTSTART', $begin->getICal());
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * @param BAB_DateTime $end
 	 * @param bab_CalendarPeriod
 	 */
 	public function setEndDate(BAB_DateTime $end) {
-	
+
 		$this->ts_end = $end->getTimeStamp();
 		$this->setProperty('DTEND', $end->getICal());
-	
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Link period to collection
 	 * @param bab_PeriodCollection $collection
@@ -165,7 +165,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	{
 		$this->collection = $collection;
 	}
-	
+
 	/**
 	 * Get period collection
 	 * @return bab_PeriodCollection
@@ -174,41 +174,41 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	{
 		return $this->collection;
 	}
-	
-	
+
+
 	/**
 	 * Get identifier used in url to identify a period
 	 * for modification of a period, the read only event will not need url identifier, so attachment to eventCalendar object is only required for modifiable events
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getUrlIdentifier()
 	{
 		$uid = $this->getProperty('UID');
-		
+
 		if (empty($uid)) {
 			throw new Exception('The UID property of period is missing, the url identifier cannot be generated');
 		}
-		
+
 		return $uid;
 	}
-	
+
 	public function setUiCalendar(bab_EventCalendar $calendar)
 	{
 		$this->uiCalendar = $calendar;
 	}
-	
+
 	public function getUiCalendar()
 	{
 		return $this->uiCalendar;
 	}
 
-	
+
 	public function setUiIdentifier($uid)
 	{
 		$this->uiIdentifier = $uid;
 	}
-	
+
 	/**
 	 * UID to use for UI action (tooltip)
 	 * @return unknown_type
@@ -218,11 +218,11 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		if (null !== $this->uiIdentifier)
 		{
 			return $this->uiIdentifier;
-		}	
-		
+		}
+
 		return $this->getProperty('UID');
 	}
-	
+
 
 
 	/**
@@ -238,16 +238,16 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	public function setData(Array $data) {
 		$this->data = array_merge($this->data, $data);
 	}
-	
-	
+
+
 	/**
-	 * call each modified or added attendees 
+	 * call each modified or added attendees
 	 * if attendee is the parent, do not call events
-	 * 
+	 *
 	 * @return bab_ICalendarObject
 	 */
 	public function commitEvent()
-	{	
+	{
 		$collection = $this->getCollection();
 		if (isset($collection))
 		{
@@ -259,34 +259,34 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		}
 
 		// commit attendees
-		
+
 		foreach($this->attendeesEvents as $urlidentifier => $method)
 		{
 			if (isset($calendar) && $urlidentifier === $calendar->getUrlIdentifier())
 			{
 				continue;
 			}
-			
+
 			if (isset($this->attendees[$urlidentifier]))
 			{
 				$calendar = $this->attendees[$urlidentifier]['calendar'];
 				$calendar->$method($this);
 			}
 		}
-		
+
 		$this->attendeesEvents = array();
-		
+
 		// commit relations
-		
+
 		foreach($this->relationsEvents as $urlidentifier => $method)
 		{
 			if (isset($calendar) && $urlidentifier === $calendar->getUrlIdentifier())
 			{
 				continue;
 			}
-			
+
 			// test for each RELTYPE : PARENT | CHILD | SIBLING
-			
+
 			foreach(array('PARENT', 'CHILD', 'SIBLING') as $reltype)
 			{
 				if (isset($this->relations[$reltype][$urlidentifier]))
@@ -299,41 +299,41 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 				}
 			}
 		}
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * @return bab_CalendarPeriod
 	 */
 	public function setAlarm(bab_CalendarAlarm $alarm) {
 		$this->alarm = $alarm;
 		return $this;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return bab_CalendarAlarm | null
 	 */
 	public function getAlarm() {
 		return $this->alarm;
 	}
-	
 
-	
-	
+
+
+
 	/**
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getColor()
 	{
 		return $this->getProperty('X-CTO-COLOR');
 	}
-	
-	
+
+
 
 	/**
 	 * get duration beetwen the two dates
@@ -342,11 +342,11 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	public function getDuration() {
 		return ($this->ts_end - $this->ts_begin);
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	/**
 	 * Add duration to timestamp
 	 * @param	int		&$timestamp
@@ -355,10 +355,10 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	private function add(&$timestamp, $duration) {
 		$timestamp = mktime(
 			date('G',$timestamp),
-			(int) date('i',$timestamp), 
-			($duration + ((int) date('s',$timestamp))), 
-			date('n',$timestamp), 
-			date('d',$timestamp), 
+			(int) date('i',$timestamp),
+			($duration + ((int) date('s',$timestamp))),
+			date('n',$timestamp),
+			date('d',$timestamp),
 			date('Y',$timestamp)
 			);
 	}
@@ -369,17 +369,17 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	 * sub-period are generated from 00:00:00 the first day of the main period
 	 * only sub-period overlapped with the main period are returned
 	 * work only with timestamps
-	 * 
+	 *
 	 * @param int		$duration   	Duration of subperiods in seconds
-	 * 
+	 *
 	 * @return bab_PeriodCollection		same class instance of the main period collection
 	 */
 	public function split($duration) {
 
-		
+
 		$classname = get_class($this->collection);
 		$return = new $classname;
-		
+
 		// first day
 		$start = bab_mktime(date('Y-m-d',$this->ts_begin));
 
@@ -402,7 +402,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 				$p->setEndDate(BAB_DateTime::fromTimeStamp($endDate));
 				$return->addPeriod($p);
 			}
-			return $return; // 1 period 
+			return $return; // 1 period
 		}
 
 		if ($this->ts_begin < $endDate) {
@@ -413,7 +413,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		}
 
 		while ($start < ($this->ts_end - $duration)) {
-			
+
 			$beginDate = $start;
 			$this->add($start, $duration);
 			$p = clone $this;
@@ -434,23 +434,23 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 
 		return $return;
 	}
-	
+
 	/**
 	 * Test the CLASS iCalendar property
 	 * @return bool
 	 */
 	public function isPublic() {
 		$class = $this->getProperty('CLASS');
-		
+
 		if ('' === $class || 'PUBLIC' === $class)
 		{
 			return true;
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/**
 	 * Test if availability of event has been overloaded by program or if the event is transparent
 	 * ex : during availability search, the event will be ignored
@@ -462,15 +462,15 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		{
 			return true;
 		}
-		
-	
+
+
 		if (isset($this->available)) {
 			return $this->available;
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Get author of event
 	 * @return int	id_user
@@ -478,15 +478,15 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	public function getAuthorId()
 	{
 		$data = $this->getData();
-		
+
 		if (isset($data['id_creator'])) {
 			return (int) $data['id_creator'];
 		}
-		
+
 		return null;
 	}
-	
-	
+
+
 	/**
 	 * the locked attribute remove modification rights for other person than the event author
 	 * return true if the locked attribute is set
@@ -498,17 +498,17 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		if (isset($data['block'])) {
 			return 'Y' === $data['block'];
 		}
-		
+
 		return false;
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
-	 * 
+	 *
 	 * @return bool
 	 */
 	public function save()
@@ -518,10 +518,10 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		$backend = $calendar->getBackend();
 		return $backend->savePeriod($this);
 	}
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * @return bool
 	 */
 	public function delete()
@@ -532,9 +532,9 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		return $backend->deletePeriod($this);
 	}
 
-	
-	
-	
+
+
+
 	/**
 	 * get HTML string for object
 	 * displayable in bab_debug
@@ -542,28 +542,28 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	 */
 	public function toHtml()
 	{
-		$html = ''; 
-		
+		$html = '';
+
 		$collection = $this->getCollection();
-		if ($collection) 
+		if ($collection)
 		{
 			$html .= '<p>Collection : '.get_class($collection).'</p>';
 			$calendar = $collection->getCalendar();
-			
+
 			if ($calendar)
 			{
 				$html .= sprintf('<p>Calendar : %s, %s</p>', $calendar->getName(), $calendar->getUrlIdentifier());
 			}
 		}
-		
-		
+
+
 		$html .= '<table class="itterable">';
 		foreach($this->getProperties() as $property)
 		{
 			if (preg_match('/(?P<name>[^:^;]+)(?P<parameters>[^:]*):(?P<value>.+)/', $property, $m))
 			{
 				$paramlist = '';
-				
+
 				if (!empty($m['parameters']))
 				{
 					$paramlist = array();
@@ -577,7 +577,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 					}
 					$paramlist = implode(';', $paramlist);
 				}
-				
+
 				$html .= '<tr>';
 				$html .= '<th>'.bab_toHtml($m['name']).'</th>';
 				$html .= '<td>'.bab_toHtml($m['value']).'</td>';
@@ -586,12 +586,12 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 			}
 		}
 		$html .= '</table>';
-		
+
 		return $html;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Get organizer informations
 	 * @return Array
@@ -599,22 +599,22 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	function getOrganizer()
 	{
 		$organizer = $this->getProperty('ORGANIZER');
-		
+
 		if (!$organizer)
 		{
 			return null;
 		}
-		
+
 		if (is_array($organizer))
 		{
 			foreach($organizer as $params => $value)
 			{
-				
+
 				list(,$email) = explode(':', $value);
 				$name = null;
 				$arrparams = explode(';', $params);
 				array_shift($arrparams);
-				
+
 				foreach($arrparams as $param)
 				{
 					if ($param)
@@ -626,35 +626,35 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 						}
 					}
 				}
-				
+
 				return array(
 					'name' => $name,
 					'email' => $email
 				);
 			}
-			
+
 		} else {
-			
-			list(,$email) = explode(':', $value);
-			
+
+			list(,$email) = explode(':', $organizer);
+
 			return array(
 				'name' => null,
 				'email' => $email
 			);
 		}
-	
+
 		return null;
 	}
-	
-	
+
+
 	/**
 	 * test if a user can view the event with a worflow instance
 	 * if event need approbation for the user, this method return true
 	 * if event need approbation from another the method return false
 	 * if event do not need approbation the method return true
-	 * 
+	 *
 	 * @param	int		$id_user
-	 * 
+	 *
 	 * @return bool
 	 */
 	public function WfInstanceAccess($id_user = null)
@@ -663,7 +663,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 		{
 			$id_user = $GLOBALS['BAB_SESS_USERID'];
 		}
-		
+
 		$relations = $this->getRelations();
 		foreach($relations as $relation)
 		{
@@ -687,7 +687,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 }
@@ -695,10 +695,10 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 
 
 /**
- * 
+ *
  */
 class bab_CalendarAlarm extends bab_ICalendarObject {
-	
+
 	public function getName() {
 		return 'VALARM';
 	}
