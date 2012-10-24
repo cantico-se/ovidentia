6053c9e8ee185cc82ff35caf2031bbc631725c9b
antgal <antgal>
2012-10-24 09:48:39 +0000

    -Fix an issue when removing a delegation, folder was only move in the database not in the file system. -Fix an other issue when removing delegation, files are now attach to dg0 in the database.

--- utilit/delegincl.php
+++ utilit/delegincl.php
@@ -453,173 +453,7 @@ function bab_getDelegationById($id)
 }
 
 
-/**
- * Groups displayed in an ACL form
- *
- */
-class bab_AclGroups
-{
 
-	private $id_delegation = 0;
-
-	/**
-	 *
-	 * @param int $id_delegation
-	 */
-	public function __construct($id_delegation)
-	{
-		$this->id_delegation = $id_delegation;
-	}
-
-
-	/**
-	 * get default group list for one level of group
-	 * using the group API
-	 * @return array
-	 */
-	private function getList($id_parent, Array $allowedChildren = null, Array $allowed = null)
-	{
-		$groups = bab_getGroups($id_parent, false);
-
-		$list = array();
-		foreach ($groups['id'] as $key => $id) {
-
-			$list[$id] = array(
-					'name'				=> $groups['name'][$key],
-					'position'			=> $groups['position'][$key],
-					'allowed'			=> (isset($allowed[$id]) || null === $allowed),					// group allowed to be checked on ACL form (right checkbox)
-					'allowedChildren'	=> (isset($allowedChildren[$id]) || null === $allowedChildren)	// group and children allowed to be checked on ACL form (left checkbox)
-			);
-
-		}
-
-		return $list;
-	}
-
-
-	/**
-	 * get one group in a level of group
-	 * @param int $id_group
-	 * return array
-	 */
-	private function getGroup($id_group, $allowedChildren = true, $allowed = true)
-	{
-		$group = bab_Groups::get($id_group);
-
-		return array(
-				$id_group => array(
-						'name'				=> $group['name'],
-						'position'			=> array('lf' => $group['lf'], 'lr' => $group['lr']),
-						'allowed'			=> $allowed,					// group allowed to be checked on ACL form (right checkbox)
-						'allowedChildren'	=> $allowedChildren				// group and children allowed to be checked on ACL form (left checkbox)
-				)
-		);
-	}
-
-
-	/**
-	 * Get allowed groups for ACL of a delegation for one level of groups
-	 * if a group template is set in table BAB_DG_ACL_GROUPS_TBL allow only the treeview defined in this template
-	 * otherwise, use the delegation group as root of allowed groups
-	 *
-	 * @param int	$id_delegation
-	 * @param int 	$id_parent
-	 *
-	 *
-	 * @return array
-	 */
-	public function getLevel($id_parent)
-	{
-		$id_delegation = $this->id_delegation;
-
-
-		if (0 == $id_delegation)
-		{
-			return $this->getList($id_parent);
-		}
-
-		global $babDB;
-
-		// get LF and LR of parent group
-
-		$parent = bab_Groups::get($id_parent);
-		$allowed = array();
-		$allowedChildren = array();
-
-
-		$res = $babDB->db_query("SELECT t.id_group, g.lf, g.lr, g.nb_groups FROM ".$babDB->backTick(BAB_DG_ACL_GROUPS_TBL)." t
-				left join ".BAB_GROUPS_TBL." g on g.id=t.id_group
-				WHERE t.id_object='".$babDB->db_escape_string($id_delegation)."'
-				");
-
-		if (0 === $babDB->db_num_rows($res))
-		{
-			// no ACL definition, get the delegation group as allowed root
-
-			list($delegation) = bab_getDelegationById($id_delegation);
-			$delegation_group = bab_Groups::get($delegation['id_group']);
-
-
-			// allow the predefined list if under delegation group
-
-			if ($id_parent == $delegation['id_group'] || ($parent['lf'] > $delegation_group['lf'] && $parent['lr'] < $delegation_group['lr']))
-			{
-				return $this->getList($id_parent);
-			}
-
-			$allowed[$delegation['id_group']] = 1;
-			$allowedChildren[$delegation['id_group']] = 1;
-
-			if ($id_parent == BAB_REGISTERED_GROUP)
-			{
-				// simplify the ACL treeview ...
-				return $this->getGroup($delegation['id_group']);
-			}
-		}
-
-
-
-
-		while ($arr = $babDB->db_fetch_assoc($res)) {
-
-			if ($arr['id_group'] >= BAB_ACL_GROUP_TREE )
-			{
-				$arr['id_group'] -= BAB_ACL_GROUP_TREE;
-
-				// if one of the left checked group is upper or equal to id_parent, return the predefined list
-
-				if ($id_parent == $arr['id_group'] || ($parent['lf'] > $arr['lf'] && $parent['lr'] < $arr['lr']))
-				{
-					return $this->getList($id_parent);
-				}
-
-				$allowed[$arr['id_group']] = 1;
-				$allowedChildren[$arr['id_group']] = 1;
-			}
-			else
-			{
-				if( $arr['nb_groups'] !== null )
-				{
-					// set of groups
-
-					$rs=$babDB->db_query("select id_group from ".BAB_GROUPS_SET_ASSOC_TBL." where id_set=".$babDB->quote($arr['id_group']));
-					while( $rr = $babDB->db_fetch_array($rs))
-					{
-						$allowed[$rr['id_group']] = 1;
-					}
-				}
-				else
-				{
-					$allowed[$arr['id_group']] = 1;
-				}
-			}
-		}
-
-		return $this->getList($id_parent, $allowedChildren, $allowed);
-
-	}
-
-}
 
 /**
  * Remove a delegation group from database
@@ -722,17 +556,42 @@ function bab_deleteDelegation($id_delegation, $deleteObjects)
 	}
 	else
 	{
+		require_once $GLOBALS['babInstallPath']."utilit/path.class.php";
+		require_once $GLOBALS['babInstallPath']."utilit/iterator/iterator.php";
+		require_once $GLOBALS['babInstallPath']."utilit/fmset.class.php";
+		$pathTo = new bab_path(BAB_FmFolderHelper::getUploadPath(), 'fileManager', 'collectives', 'DG0');
+		$pathsFrom = new bab_path(BAB_FmFolderHelper::getUploadPath(), 'fileManager', 'collectives', 'DG'.$idsafe);
+		
+		$moveArray = array();
+		/* @var $pathFrom bab_path */
+		foreach($pathsFrom as $pathFrom){
+			if($pathFrom->isDir()){
+				$dirname = $pathFrom->getBasename();
+				$pathsToExist = new bab_path($pathTo->tostring(),$dirname);
+				if($pathsToExist->fileExists()){
+					throw new ErrorException('Delete not done, a folder with this name already exist');
+				}
+				$moveArray[] = array('from' => $pathFrom->tostring(), 'to' => $pathsToExist->tostring());
+			}
+		}
+		
+		foreach($moveArray as $move){
+			rename($move['from'], $move['to']);
+		}
+		
 		$babDB->db_query("update ".BAB_SECTIONS_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_TOPICS_CATEGORIES_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_FLOW_APPROVERS_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_FORUMS_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_FAQCAT_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_FM_FOLDERS_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
+		$babDB->db_query("update ".BAB_FILES_TBL." set iIdDgOwner='0' where iIdDgOwner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_LDAP_DIRECTORIES_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_DB_DIRECTORIES_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_ORG_CHARTS_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_CAL_RESOURCES_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
 		$babDB->db_query("update ".BAB_CAL_PUBLIC_TBL." set id_dgowner='0' where id_dgowner='".$idsafe."'");
+	
 	}
 	
 	
