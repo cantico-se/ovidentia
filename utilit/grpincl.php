cae0f84a479194c24a8afb21b4f587e5dcbbe0a2
pauder <pauder>
2013-01-23 15:15:51 +0000

    add a clear cache when a new group is added

--- utilit/grpincl.php
+++ utilit/grpincl.php
@@ -236,6 +236,9 @@ function bab_addGroup($name, $description, $managerid, $grpdg, $parent = 1)
 				id='".$babDB->db_escape_string($id)."'
 			");
 		
+		include_once $GLOBALS['babInstallPath']."utilit/groupsincl.php";
+		bab_Groups::clearCache();
+		
 		include_once $GLOBALS['babInstallPath'].'utilit/addonsincl.php';
 		bab_callAddonsFunction('onGroupCreate', $id);
 		return $id;
