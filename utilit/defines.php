7fcc7e792326ca307cc7c68374887b4e76432dc0
pauder <pauder>
2013-07-10 08:46:35 +0000

    remove unused constants

--- utilit/defines.php
+++ utilit/defines.php
@@ -328,11 +328,7 @@ define("BAB_USERS_GROUPS_TBL", "bab_users_groups");
 define("BAB_USERS_LOG_TBL", "bab_users_log");
 define("BAB_USERS_UNAVAILABILITY_TBL", "bab_users_unavailability");
 define("BAB_UPGRADE_MESSAGES_TBL", "bab_upgrade_messages");
-define("BAB_VACATIONS_TBL", "bab_vacations");
-define("BAB_VACATIONS_STATES_TBL", "bab_vacations_states");
-define("BAB_VACATIONS_TYPES_TBL", "bab_vacations_types");
-define("BAB_VACATIONSMAN_GROUPS_TBL", "bab_vacationsman_groups");
-define("BAB_VACATIONSVIEW_GROUPS_TBL", "bab_vacationsview_groups");
+
 define("BAB_VAC_MANAGERS_TBL", "bab_vac_managers");
 define("BAB_VAC_TYPES_TBL", "bab_vac_types");
 define("BAB_VAC_COLLECTIONS_TBL", "bab_vac_collections");
