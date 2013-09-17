f2ca7dbb22979cce2e9bad388627fbe8dca6b894
pauder <pauder>
2013-09-17 07:10:10 +0000

    T7236 event is deleted but not created because of  an error message

--- utilit/cal.calendarperiod.class.php
+++ utilit/cal.calendarperiod.class.php
@@ -742,7 +742,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 			
 			if (false === $backend->savePeriod($this, 'CANCEL'))
 			{
-				$failure[] = $calendar->getName();
+				// $failure[] = $calendar->getName();
 			}
 			
 		}
