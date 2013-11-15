146999fa40a95b8b5482a0163c6b8f29dc4b0fba
laucho <laucho>
2013-11-15 10:47:31 +0000

    Added documentation on all bab_WFxxx functions.

--- utilit/wfincl.php
+++ utilit/wfincl.php
@@ -20,29 +20,77 @@
  * along with this program; if not, write to the Free Software			*
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
  * USA.																	*
-************************************************************************/
+ ************************************************************************/
 /**
 * @internal SEC1 NA 05/12/2006 FULL
 */
 include_once 'base.php';
 include_once $GLOBALS['babInstallPath'].'utilit/afincl.php';
 
-function bab_WFMakeInstance($idsch, $extra, $user=0)
+
+
+
+/**
+ * Creates an instance of approbation schema with id $idsch and return the id of the instance.
+ * 
+ * @param int		$idsch	Id of the approbation schema.
+ * @param string	$extra	Instance identification string. This information id not used by Ovidentia, can be used debugging purpose.
+ * @param int		$user	User id of owner for auto-approbation, 0 = no auto-approbation.
+ * 
+ * @return int		The id of the new instance.
+ */
+function bab_WFMakeInstance($idsch, $extra, $user = 0)
 {
 	return makeFlowInstance($idsch, $extra, $user);
 }
 
+
+
+
+
+/**
+ * Deletes the specified instance.
+ * 
+ * @param int		$idschi		Id of the instance.
+ */
 function bab_WFDeleteInstance($idschi)
 {
 	return deleteFlowInstance($idschi);
 }
 
+
+
+
+
+/**
+ * Updates an instance with user’s response.
+ * 
+ * @param int		$idschi		Id of the instance.
+ * @param int		$iduser		User id.
+ * @param bool		$bool		True if the user accept the approbation and false if the user decline the approbation.
+ * 
+ * @return number	The new result of the schema approbation:
+ *						- 0 if the approbation is declined and then the subject of the approbation must be revoked.
+ * 						- 1 if the approbation is accepted and hence the subject of the approbation must be accepeted.
+ */
 function bab_WFUpdateInstance($idschi, $iduser, $bool)
 {
 	return updateFlowInstance($idschi, $iduser, $bool);
 }
 
-function bab_WFCheckInstance($idsa, $iduser, $update=false)
+
+
+
+
+/**
+ * This function return true if there is a waiting approbation instance for the user with id $iduser.
+ * 
+ * @param int	$idsa		Id of the approbation schema.
+ * @param int 	$iduser		User id.
+ * @param bool	$update		If true the session cache will not be used.
+ * @return boolean
+ */
+function bab_WFCheckInstance($idsa, $iduser, $update = false)
 {
 
 	$arr = getWaitingApprobations($iduser, $update);
@@ -57,12 +105,34 @@ function bab_WFCheckInstance($idsa, $iduser, $update=false)
 }
 
 
-function bab_WFGetWaitingApproversInstance($idschi, $notify=false)
+
+
+
+/**
+ * Returns an array with users ids who wait for approbation schema instance with id $idschi.
+ * 
+ * @param int	$idschi		The approbation schema id.
+ * @param bool	$notify		If true, the mark waiting users to be as notified.
+ * @return array
+ */
+function bab_WFGetWaitingApproversInstance($idschi, $notify = false)
 {
-	return getWaitingApproversFlowInstance($idschi, $notify);
+	$result = getWaitingApproversFlowInstance($idschi, $notify);
+	return $result;
 }
 
-function bab_WFGetWaitingInstances($iduser, $update=false)
+
+
+
+
+/**
+ * Returns an array of all waiting instances.
+ * 
+ * @param int	$iduser
+ * @param bool	$update		If true the session cache will not be used.
+ * @return array			An array of instances id.
+ */
+function bab_WFGetWaitingInstances($iduser, $update = false)
 {
 	$arr = getWaitingApprobations($iduser, $update);
 	$result = array();
@@ -74,8 +144,18 @@ function bab_WFGetWaitingInstances($iduser, $update=false)
 }
 
 
+
+
+
 /**
- * 
+ * Returns the list of list of approbation schema.
+ * The returned array has the following structure:
+ * array(
+ * 		0 => array('name' = first schema name, 'id' = first schema id),
+ *  	...
+ *  	n => array('name' = nth schema name, 'id' = nth schema id)
+ * )
+ *
  * @return array
  */
 function bab_WFGetApprobationsList()
@@ -89,6 +169,10 @@ function bab_WFGetApprobationsList()
 	}
 	return $result;
 }
+
+
+
+
 
 /**
  * Get approbationscheme information
