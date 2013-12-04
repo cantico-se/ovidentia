0e2a18d799d35448a81e4b2ba359543fad840614
laucho <laucho>
2013-12-04 10:32:15 +0000

    Stats on external links now use Applications functionality to give the application name if available.

--- statxlink.php
+++ statxlink.php
@@ -26,36 +26,33 @@ include_once $babInstallPath."utilit/statutil.php";
 include_once $babInstallPath."utilit/uiutil.php";
 
 function summaryXlinks($col, $order, $startday, $endday)
-	{
+{
 	global $babBody;
 	class summaryXlinksCls extends summaryBaseCls
-		{
+	{
 		var $fullname;
 		var $res;
 		var $count;
 		var $url;
 		var $urlname;
 		var $altbg = true;
+		
+		var $Applications = null;
 
 		function summaryXlinksCls($col, $order, $startday, $endday)
-			{
+		{
 			global $babBody, $babDB;
 			$this->summaryBaseCls();
 			$this->fullname = bab_translate("External links");
 			$this->hitstxt = bab_translate("Hits");
 			$req = "SELECT st_xlink_url , sum(st_hits) hits FROM  ".BAB_STATS_XLINKS_TBL."";
-			if( !empty($startday) && !empty($endday))
-				{
+			if (!empty($startday) && !empty($endday)) {
 				$req .= " where st_date between '".$startday."' and '".$endday."'";
-				}
-			else if( !empty($startday))
-				{
+			} else if (!empty($startday)) {
 				$req .= " where st_date >= '".$startday."'";
-				}
-			else if( !empty($endday))
-				{
+			} else if (!empty($endday)) {
 				$req .= " where st_date <= '".$endday."'";
-				}
+			}
 
 			$req .= " GROUP  by st_xlink_url";
 
@@ -66,14 +63,13 @@ function summaryXlinks($col, $order, $startday, $endday)
 			$this->sortcol = $col;
 			$this->totalhits = 0;
 			$this->arrinfo = array();
-			while($arr = $babDB->db_fetch_array($res))
-				{
+			while ($arr = $babDB->db_fetch_array($res)) {
 				$tmparr = array();
 				$tmparr['link'] = $arr['st_xlink_url'];
 				$tmparr['hits'] = $arr['hits'];
 				$this->arrinfo[] = $tmparr;
 				$this->totalhits += $tmparr['hits'];
-				}
+			}
 
 			$this->totalhitspc = 100;
 
@@ -82,74 +78,73 @@ function summaryXlinks($col, $order, $startday, $endday)
 
 			$this->urlordmod = "idx=xlink&order=".($col == 'link'? $this->sortord: $order)."&col=link";
 			$this->urlordhits = "idx=xlink&order=".($col == 'hits'? $this->sortord: $order)."&col=hits";
+			
+			$this->Applications = bab_functionality::get('Applications');
+			if (!method_exists($this->Applications, 'getApplicationByUrl')) {
+			    $this->Applications = null;
 			}
+		}
 
 		function isNumeric($col)
-			{
-			switch( $this->sortcol )
-				{
+		{
+			switch ($this->sortcol) {
 				case 'hits':
 					return true;
 				default:
 					return false;
-				}
 			}
+		}
 
 		function getnext()
-			{
+		{
 			global $babDB;
 			static $i = 0;
-			if( $i < $this->count)
-				{
+			if ($i < $this->count) {
 				$this->altbg = $this->altbg ? false : true;
-				$this->modulename = $this->arrinfo[$i]['link'];
+				$this->moduleurl = $this->arrinfo[$i]['link'];
 				$this->nbhits = $this->arrinfo[$i]['hits'];
 				$this->nbhitspc = $this->totalhits > 0 ? round(($this->nbhits*100)/$this->totalhits,2): 0;
 				$taille=($this->nbhits*100)/$this->totalhits;
 				$this->size=$taille;
 				$this->size2=100-$taille;
+				
+				if ($this->Applications && $application = $this->Applications->getApplicationByUrl($this->arrinfo[$i]['link'])) {
+				    $this->modulename = $application->shortname;
+				} else {
+				    $this->modulename = '';
+				}
 				$i++;
 				return true;
-				}
-			else
-				return false;
+			}
+			return false;
 
-			}
 		}
+	}
+
 	$temp = new summaryXlinksCls($col, $order, $startday, $endday);
-	if( isset($GLOBALS['export']) && $GLOBALS['export'] == 1 )
-		{
+	if (isset($GLOBALS['export']) && $GLOBALS['export'] == 1) {
 		$output = bab_translate("External links");
-		if( !empty($startday) && !empty($endday))
-			{
+		if (!empty($startday) && !empty($endday)) {
 			$output .= " (".bab_strftime(bab_mktime($startday." 00:00:00"), false)." - ".bab_strftime(bab_mktime($endday." 00:00:00"), false).")";
-			}
-		else if( !empty($startday))
-			{
+		} else if( !empty($startday)) {
 			$output .= " (".bab_strftime(bab_mktime($startday." 00:00:00"), false)." - )";
-			}
-		else if( !empty($endday))
-			{
+		} else if (!empty($endday)) {
 			$output .= " ( - ".bab_strftime(bab_mktime($endday." 00:00:00"), false).")";
-			}
+		}
 		$output .= " - ".bab_translate("Total: ").$temp->totalhits;
 		$output .= "\n";
 		$output .= $temp->fullname.$GLOBALS['exportchr'].$temp->hitstxt.$GLOBALS['exportchr']."%\n";
-		while($temp->getnext())
-			{
-			$output .= $temp->modulename.$GLOBALS['exportchr'].$temp->nbhits.$GLOBALS['exportchr'].$temp->nbhitspc."\n";
-			}
+		while ($temp->getnext()) {
+			$output .= $temp->moduleurl.$GLOBALS['exportchr'].$temp->nbhits.$GLOBALS['exportchr'].$temp->nbhitspc."\n";
+		}
 		header("Content-Disposition: attachment; filename=\"export.csv\""."\n");
 		header("Content-Type: text/plain"."\n");
 		header("Content-Length: ". mb_strlen($output)."\n");
 		header("Content-transfert-encoding: binary"."\n");
 		print $output;
 		exit;
-		}
-	else
-		{
+	} else {
 		$babBody->babecho(	bab_printTemplate($temp, "statxlink.html", "summaryxlinkslist"));
 		return $temp->count;
-		}
 	}
-?>
+}
