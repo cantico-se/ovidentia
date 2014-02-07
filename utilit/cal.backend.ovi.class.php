22bf59d3f37e9134c656e4806ea2053168a5a828
pauder <pauder>
2014-02-07 10:49:07 +0000

    T7728 Fix save and delete in multiple collections

--- utilit/cal.backend.ovi.class.php
+++ utilit/cal.backend.ovi.class.php
@@ -96,25 +96,29 @@ class Func_CalendarBackend_Ovi extends Func_CalendarBackend
 		
 		if ($collection->hash)
 		{
+			$status = true;
 			foreach($collection as $period)
 			{
+
 				// ovidentia backend does not support method CANCEL, so we delete if the event is canceled
 				if ('CANCEL' === $method)
 				{
 					if (!$this->deletePeriod($period))
 					{
-						return false;
+						$status = false;
+						continue;
 					}
 					
 				} else {
 				
 					if (!bab_cal_OviEventUpdate::save($period))
 					{
-						return false;	
+						$status = false;
+						continue;	
 					}
 				}
 			}
-			return true;
+			return $status;
 		
 		} else {
 			
@@ -236,6 +240,7 @@ class Func_CalendarBackend_Ovi extends Func_CalendarBackend
 		require_once dirname(__FILE__).'/cal.ovievent.class.php';
 		$oviEvents = new bab_cal_OviEventSelect;
 		
+		bab_debug('DELETE : '.$period->getProperty('UID'), bab_shortDate($period->ts_begin));
 		
 		return $oviEvents->deleteFromUid($period->getProperty('UID'), $period->ts_begin);
 	}
