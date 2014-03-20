8b6ff6bb6eb41674c189097ae0eed71a03586e54
pauder <pauder>
2014-03-20 15:33:55 +0000

    translate

--- distrib/install/babinstall.sql
+++ distrib/install/babinstall.sql
@@ -729,7 +729,7 @@ CREATE TABLE bab_users (
    KEY hashname (hashname)
 );
 
-INSERT INTO bab_users VALUES ( '1', 'admin@admin.bab', 'Administrator', 'Ovidentia', '200ceb26807d6bf99fd6f4f0d1ca54d4', 'admin@admin.bab', '2001-04-03 00:00:00', '22975d8a5ed1b91445f6c55ac121505b', '1', '', '0da8f2a37b9e7926e08196a6bd1baa29', '1', '0', '0000-00-00', '0000-00-00', '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', '', '', '','N','0000-00-00 00:00:00','','0');
+INSERT INTO bab_users VALUES ( '1', 'admin@admin.bab', 'Administrateur', 'Ovidentia', '200ceb26807d6bf99fd6f4f0d1ca54d4', 'admin@admin.bab', '2001-04-03 00:00:00', '22975d8a5ed1b91445f6c55ac121505b', '1', '', '0da8f2a37b9e7926e08196a6bd1baa29', '1', '0', '0000-00-00', '0000-00-00', '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0', '', '', '','N','0000-00-00 00:00:00','','0');
 
 # --------------------------------------------------------
 #
