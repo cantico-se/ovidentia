08f5e8cc0234c58f7502dbf6982d6ed592ceffba
pauder <paul.derosanbo@cantico.fr>
2014-10-03 10:12:07 +0200

    Remove include_once base.php only in the utilit folder

--- utilit/tableWrapperClass.php
+++ utilit/tableWrapperClass.php
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,*
  * USA.																	*
 ************************************************************************/
-	include "base.php";
+	
 
 	DEFINE('BAB_NONE', -1);
 	DEFINE('BAB_ALL_ATTRIBUTS', -2);
