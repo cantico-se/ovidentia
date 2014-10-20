b452255b18715d30e3a286f2a4e1014df0cc2061
pauder <paul.derosanbo@cantico.fr>
2014-10-20 14:10:05 +0200

    session alternative, contain a mock storage

--- utilit/session.class.php
+++ utilit/session.class.php
@@ -23,63 +23,185 @@
 ************************************************************************/
 
 
+interface bab_SessionStorage
+{
+    /**
+     * Return session ID or empty string if no session exists
+     * @return string
+     */
+    public function getSessionId();
+    public function start();
+    
+
+    public function __set($name , $value);
+    
+    public function __get($name);
+    
+    public function __isset($name);
+    
+    public function __unset($name);
+}
+
+
+class bab_SessionMockStorage implements bab_SessionStorage
+{
+
+    /**
+     * @var string
+     */
+    private $mockId = '';
+    
+    private $values;
+    
+    /**
+     * @return string
+     */
+    public function getSessionId()
+    {
+        return $this->mockId;
+    }
+    
+    public function start()
+    {
+        $this->mockId = md5(uniqid('', true));
+        $this->values = array();
+    }
+    
+
+    public function __set($name , $value)
+    {
+        $this->values[$name] = $value;
+    }
+    
+    public function __get($name)
+    {
+        return $this->values[$name];
+    }
+    
+    public function __isset($name)
+    {
+        return array_key_exists($name, $this->values);
+    }
+    
+    public function __unset($name)
+    {
+        unset($this->values[$name]);
+    }
+    
+}
+
+
+class bab_SessionDefaultStorage implements bab_SessionStorage
+{
+    /**
+     * @return string 
+     */
+    public function getSessionId()
+    {
+        return session_id();
+    }
+    
+    public function start()
+    {
+        global $babUrl;
+        session_name(sprintf("OV%u", crc32($babUrl)));
+        session_start();
+    }
+    
+    
+
+    public function __set($name , $value)
+    {
+        $_SESSION[$name] = $value;
+    }
+    
+    public function __get($name)
+    {
+        return $_SESSION[$name];
+    }
+    
+    public function __isset($name)
+    {
+        return array_key_exists($name, $_SESSION);
+    }
+    
+    public function __unset($name)
+    {
+        unset($_SESSION[$name]);
+    }
+}
+
+
+
+
 
 class bab_Session
 {
-	
+    /**
+     * @var bab_SessionStorage
+     */
+    private $storage;
+    
+
+    /**
+     * 
+     */
+    public function setStorage(bab_SessionStorage $storage)
+    {
+        $this->storage = $storage;
+    }
+    
+    /**
+     * Get storage instance and start session if not started
+     * @return bab_SessionStorage
+     */
+    protected function getStorage()
+    {
+        if (!isset($this->storage)) {
+            $this->storage = new bab_SessionDefaultStorage();
+        }
+        
+        if (!$this->storage->getSessionId()) {
+            $this->storage->start();
+        }
+        
+        return $this->storage;
+    }
+
+    /**
+     * Start session
+     */
 	public function start()
 	{
-		global $babUrl;
-		
-		session_name(sprintf("OV%u", crc32($babUrl)));
-		session_start();
+	    return $this->storage->start();
 	}
+	
 	
 	/**
 	 * @return string
 	 */
 	public function getId()
 	{
-		if (!session_id()) {
-			$this->start();
-		}
-		
-		return session_id();
+		return $this->getStorage()->getSessionId();
 	}
 	
 	public function __set($name , $value)
 	{
-		if (!session_id()) {
-			$this->start();
-		}
-		
-		$_SESSION[$name] = $value;
+		$this->getStorage()->__set($name, $value);
 	}
 	
 	public function __get($name)
 	{
-		if (!session_id()) {
-			$this->start();
-		}
-		
-		return $_SESSION[$name];
+		return $this->getStorage()->__get($name);
 	}
 	
 	public function __isset($name)
 	{
-		if (!session_id()) {
-			$this->start();
-		}
-		
-		return array_key_exists($name, $_SESSION);
+		return $this->getStorage()->__isset($name);
 	}
 	
 	public function __unset($name)
 	{
-		if (!session_id()) {
-			$this->start();
-		}
-		
-		unset($_SESSION[$name]);
+		return $this->getStorage()->__unset($name);
 	}
 }
