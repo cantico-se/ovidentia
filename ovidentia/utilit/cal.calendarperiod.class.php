77bd828a7b20a5d7aaf4ab4c79ad8ff3dd19bdd5
Paul de Rosanbo <paul.derosanbo@cantico.fr>
2016-02-01 15:26:06 +0100

    Access to approval details if the user is an approver, give access also if not notified (status in workflow api)

--- ovidentia/utilit/cal.calendarperiod.class.php
+++ ovidentia/utilit/cal.calendarperiod.class.php
@@ -658,6 +658,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 			'email' => $email
 		);
 	}
+	
 
 
 	/**
@@ -674,7 +675,7 @@ class bab_CalendarPeriod extends bab_ICalendarObject {
 	{
 		if (null === $id_user)
 		{
-			$id_user = $GLOBALS['BAB_SESS_USERID'];
+			$id_user = bab_getUserId();
 		}
 		
 		if (!$id_user)
