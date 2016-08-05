5bec53de93a716b39a677e618d686c8bdb40d507
Paul de Rosanbo <paul.derosanbo@cantico.fr>
2016-08-05 16:46:03 +0200

    fix SCRIPT_NAME from router.php

--- router.php
+++ router.php
@@ -3,12 +3,14 @@
 // router file for rewriting with the internal php server
 // $php -S localhost:8080 router.php
 
+$__url = parse_url($_SERVER["REQUEST_URI"]);
 
-if (file_exists($_SERVER["DOCUMENT_ROOT"] . urldecode($_SERVER["SCRIPT_NAME"]))) {
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . urldecode($__url['path']))) {
     return false;
 }
 
-$__url = parse_url($_SERVER["REQUEST_URI"]);
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 if ('/' !== $__url['path']) {
 	$_GET['babrw'] = $__url['path'];
 }
