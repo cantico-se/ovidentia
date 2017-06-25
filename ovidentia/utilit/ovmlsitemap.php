404a0834cd5496f74c4c97331ac5d6f8797f54ca
Antoine GALLET <antoine.gallet@cantico.fr>
2017-06-25 15:37:18 +0000

    Rollback commit.

--- ovidentia/utilit/ovmlsitemap.php
+++ ovidentia/utilit/ovmlsitemap.php
@@ -190,10 +190,6 @@ abstract class Ovml_Container_Sitemap extends Func_Ovml_Container
             $this->idx = $this->limitOffset;
             return false;
         }
-        
-        if (!isset($this->IdEntries[$this->idx])) {
-            return false;
-        }
 
         $entry = $this->IdEntries[$this->idx];
 
@@ -204,7 +200,6 @@ abstract class Ovml_Container_Sitemap extends Func_Ovml_Container
         $this->ctx->curctx->push('SitemapEntryId'                  , $entry['id']);
         $this->ctx->curctx->push('SitemapEntryOnclick'             , $entry['onclick']);
         $this->ctx->curctx->push('SitemapEntryFolder'              , $entry['folder'] ? '1' : '0');
-        $this->ctx->curctx->push('SitemapEntryHasChildNodes'       , $entry['hasChildNodes'] ? '1' : '0');
         $this->ctx->curctx->push('SitemapEntryPageTitle'           , $entry['pageTitle']);
         $this->ctx->curctx->push('SitemapEntryPageDescription'     , $entry['pageDescription']);
         $this->ctx->curctx->push('SitemapEntryPageKeywords'        , $entry['pageKeywords']);
@@ -321,7 +316,6 @@ class Func_Ovml_Container_SitemapEntry extends Ovml_Container_Sitemap
                 $tmp['id'] = $item->id_function;
                 $tmp['onclick'] = $item->onclick;
                 $tmp['folder'] = $item->folder;
-                $tmp['hasChildNodes'] = $item->hasChildNodes(); 	// has childnodes in sitemap
                 $tmp['pageTitle'] = $item->getPageTitle();
                 $tmp['pageDescription'] = $item->getPageDescription();
                 $tmp['pageKeywords'] = $item->getPageKeywords();
@@ -400,7 +394,6 @@ class Func_Ovml_Container_SitemapCustomNode extends Ovml_Container_Sitemap
                 $tmp['id'] = $item->id_function;
                 $tmp['onclick'] = $item->onclick;
                 $tmp['folder'] = $item->folder;
-                $tmp['hasChildNodes'] = $item->hasChildNodes(); 	// has childnodes in sitemap
                 $tmp['pageTitle'] = $item->getPageTitle();
                 $tmp['pageDescription'] = $item->getPageDescription();
                 $tmp['pageKeywords'] = $item->getPageKeywords();
@@ -524,7 +517,6 @@ class Func_Ovml_Container_SitemapPath extends Ovml_Container_Sitemap
                     }
                     $tmp['onclick'] = $item->onclick;
                     $tmp['folder'] = $item->folder;
-                    $tmp['hasChildNodes'] = $item->hasChildNodes(); 	// has childnodes in sitemap
                     $tmp['pageTitle'] = $item->getPageTitle();
                     $tmp['pageDescription'] = $item->getPageDescription();
                     $tmp['pageKeywords'] = $item->getPageKeywords();
